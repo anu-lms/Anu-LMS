b9c99fb57870b9246577f994fe0b5199cc770095
spleshka <spleshka@gmail.com>
2018-01-06 17:36:35 +0300

    Enhanced basic frontend application.

--- reactjs/pages/index.js
+++ reactjs/pages/index.js
@@ -1,28 +1,20 @@
 import React from 'react';
-import superagent from '../lib/request';
-import App from '../components/App/index';
+import request from '../lib/request';
+import App from '../components/App';
 import HtmlHead from '../components/HtmlHead';
-import ErrorPage from '../components/ErrorPage';
 
-class FrontPage {
-  render() {
-    return (
-      <App>
+const FrontPage = ({ metaData, analytics, favicon }) => (
+  <App>
 
-        <HtmlHead
-          metaData={this.props.metaData}
-          analytics={this.props.analytics}
-          favicon={this.props.favicon}
-        />
+    <HtmlHead
+      metaData={metaData}
+      analytics={analytics}
+      favicon={favicon}
+    />
 
-        { this.props.statusCode && this.props.statusCode !== 200
-          ? <ErrorPage code={this.props.statusCode} />
-          : <div>Home page</div>
-        }
+    <div>Home page</div>
 
-      </App>
-    );
-  }
-}
+  </App>
+);
 
 export default FrontPage;
