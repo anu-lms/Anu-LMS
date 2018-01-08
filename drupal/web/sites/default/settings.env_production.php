97ec32bd904b22e46fc468c316e6b06917864cc8
spleshka <spleshka@gmail.com>
2018-01-08 20:26:15 +0300

    [#154081636] Updated Drupal settings.

---
+++ drupal/web/sites/default/settings.env_production.php
@@ -0,0 +1,15 @@
+<?php
+
+// Example production settings.
+
+// Disable development configs.
+$config['config_split.config_split.development']['status'] = FALSE;
+$config['config_split.config_split.production']['status'] = TRUE;
+
+// Don't show errors on live.
+$config['system.logging']['error_level'] = 'hide';
+
+// Enable css / js aggregation.
+$config['system.performance']['css']['preprocess'] = TRUE;
+$config['system.performance']['js']['preprocess'] = TRUE;
+
