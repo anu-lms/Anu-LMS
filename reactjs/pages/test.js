b534f0b1ef1a9943f948dce61ee1790a5d772cb0
spleshka <spleshka@gmail.com>
2018-01-15 15:55:32 +0300

    [#154083156] Moved test to the separate page.

---
+++ reactjs/pages/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from '../application/App';
+import Test from '../components/atoms/Test';
+
+const Page = () => (
+  <App>
+    <Test />
+  </App>
+);
+
+export default Page;
