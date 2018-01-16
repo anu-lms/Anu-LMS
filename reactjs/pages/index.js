9a26e41c8172576df3fd424cb3ddcbcf90f37d5e
spleshka <spleshka@gmail.com>
2018-01-16 10:34:53 +0300

    [#154083112] Mainly finished styling for the front page.

--- reactjs/pages/index.js
+++ reactjs/pages/index.js
@@ -21,40 +21,42 @@ const FrontPage = () => (
         </div>
       </div>
 
-      <div className="row justify-content-center content">
-
-        <div className="col col-12">
+      <div className="row content">
+        <div className="col col-12 col-lg-6">
           <h1 className="heading">
             This is your tagline.<br/>
             Learn your way, your pace.
           </h1>
         </div>
 
-        <div className="col col-12 login-block">
-          <LoginForm />
+        <div className="col col-12 col-lg-6">
+          <div className="login-block">
+            <LoginForm />
 
-          <a className="forgot-password" href="#">
-            Forgot username or password?
-          </a>
+            <a className="forgot-password" href="#">
+              Forgot username or password?
+            </a>
 
-          <Separator />
+            <Separator />
 
-          <Button block type={'secondary'}>
-            New? Register here
-          </Button>
+            <Button block type={'secondary'}>
+              New? Register here
+            </Button>
+          </div>
         </div>
 
       </div>
 
-
       <div className="row footer">
-        <div className="col">
+        <div className="col-12">
           <ul className="links">
             <li className="d-inline-block"><a href="#">About Us</a></li>
             <li className="d-inline-block"><a href="#">Our Partners</a></li>
             <li className="d-inline-block"><a href="#">Privacy</a></li>
             <li className="d-inline-block"><a href="#">Legal</a></li>
           </ul>
+        </div>
+        <div className="col-12">
           <p>Copyright 2018 Anu © All Rights Reserved</p>
         </div>
       </div>
