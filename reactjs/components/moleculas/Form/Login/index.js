22c431bef0713be2b947d7c5ce8451473c8b016d
spleshka <spleshka@gmail.com>
2018-01-16 15:48:28 +0300

    [#154083112] Added initial authentication implementation.

--- reactjs/components/moleculas/Form/Login/index.js
+++ reactjs/components/moleculas/Form/Login/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Form from '../../../atoms/Form';
 import Button from '../../../atoms/Button';
+import request from '../../../../utils/request';
+import { saveAuthData } from '../../../../utils/authentication';
 
 const schema = {
   'type': 'object',
@@ -29,15 +31,67 @@ const uiSchema = {
 
 class LoginForm extends React.Component {
 
-  render() {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      isSending: false,
+      formData: {},
+    };
+
+    this.submitForm.bind(this);
+  }
+
+  submitForm({ formData }) {
+
+    this.setState({
+      isSending: true,
+      formData,
+    });
+
+    request
+      .post('/oauth/token')
+      .send({
+        'grant_type': 'password',
+        // TODO: Move to constants.
+        'client_id': '9e0c1ed1-541b-45da-9360-8b41f206352c',
+        'client_secret': '9uGSd3khRDf3bxQR',
+        'scope': '',
+        'username': formData.username,
+        'password': formData.password,
+      })
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .end((err, res) => {
+        this.setState({ isSending: false });
+        if (!err) {
+          saveAuthData(res.body.access_token, res.body.refresh_token, Date.now() + res.body.expires_in);
+          this.setState({ formData: {} });
+
+          // TODO: Remove.
+          alert('Successfull authentication!');
+        }
+        else {
+          // TODO: Handle error.
+          console.log(err);
+          console.log(res);
+          // TODO: Remove.
+          alert(res.body.message);
+        }
+      });
+  }
+
+  render() {
     return(
       <Form
         schema={schema}
         uiSchema={uiSchema}
+        formData={this.state.formData}
         autocomplete={'off'}
+        onSubmit={this.submitForm.bind(this)}
       >
-        <Button block>Login</Button>
+        <Button block loading={this.state.isSending}>
+          Login
+        </Button>
       </Form>
     );
   }
