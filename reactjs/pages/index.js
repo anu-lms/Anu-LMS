8b8486912ee6d432a53abc36d44d24791be74028
spleshka <spleshka@gmail.com>
2018-01-16 09:42:45 +0300

    [#154083112] Enhanced login page styling.

--- reactjs/pages/index.js
+++ reactjs/pages/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import App from '../application/App';
-import Test from '../components/atoms/Test';
 import LoginForm from '../components/moleculas/Form/Login';
+import Button from '../components/atoms/Button';
+import Separator from '../components/atoms/Separator';
 import { isLogged } from '../utils/authentication';
 
 const FrontPage = () => (
@@ -14,34 +15,47 @@ const FrontPage = () => (
     { !isLogged() &&
     <div className="container-fluid page-login">
 
-      <div className="row content">
-        <div className="overlay" />
+      <div className="row header">
         <div className="col">
+          <img src={"/static/img/logo.png"} />
+        </div>
+      </div>
 
-          <img className="logo" src="/static/img/logo.png" />
+      <div className="row justify-content-center content">
 
+        <div className="col col-12">
           <h1 className="heading">
             This is your tagline.<br/>
             Learn your way, your pace.
           </h1>
-
-          <div className="login-block">
-            <LoginForm />
-          </div>
-
-
         </div>
+
+        <div className="col col-12 login-block">
+          <LoginForm />
+
+          <a className="forgot-password" href="#">
+            Forgot username or password?
+          </a>
+
+          <Separator />
+
+          <Button block type={'secondary'}>
+            New? Register here
+          </Button>
+        </div>
+
       </div>
+
 
       <div className="row footer">
         <div className="col">
-         <ul className="links">
-           <li className="d-inline"><a href="#">About Us</a></li>
-           <li className="d-inline"><a href="#">Our Partners</a></li>
-           <li className="d-inline"><a href="#">Privacy</a></li>
-           <li className="d-inline"><a href="#">Legal</a></li>
-         </ul>
-          <p>Copyright 2018 Anu (c) All Rights Reserved</p>
+          <ul className="links">
+            <li className="d-inline-block"><a href="#">About Us</a></li>
+            <li className="d-inline-block"><a href="#">Our Partners</a></li>
+            <li className="d-inline-block"><a href="#">Privacy</a></li>
+            <li className="d-inline-block"><a href="#">Legal</a></li>
+          </ul>
+          <p>Copyright 2018 Anu © All Rights Reserved</p>
         </div>
       </div>
 
