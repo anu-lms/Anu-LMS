fb80cc214f957bc6eaa65f19a41d46e0d22ff0fb
kalabro <kalabro@ya.ru>
2018-01-17 19:01:41 +0300

    [#154083131] Broke markup into components.

--- reactjs/components/organisms/StudentDashboard/index.js
+++ reactjs/components/organisms/StudentDashboard/index.js
@@ -1,5 +1,83 @@
 import React, { Component } from 'react';
 
+const colorPrimary = '#12b3a4';
+const colorWhiteGrayAlternative = '#eaeaea';
+
+// Render progress bar.
+class ProgressBar extends Component {
+
+  render() {
+    const progressPercent = this.props.progressPercent;
+
+    return (
+      <div className="progress-bar__small" style={{
+        backgroundImage: `linear-gradient(90deg, ${colorPrimary} ${progressPercent}%, ${colorWhiteGrayAlternative} ${progressPercent}%)`
+      }} title={`${progressPercent}% complete`}></div>
+    );
+  }
+}
+
+class CoursesList extends Component {
+
+  // Demo data (to be fetched from backend).
+  courses = [
+    {
+      title: 'Giant Toolshed',
+      url: 'course/giant-toolshed',
+      imageUrl: 'https://spaceholder.cc/389x292',
+      imageAlt: 'Giant Toolshed',
+      progressPercent: 70
+    },
+    {
+      title: 'Giant Toolshed',
+      url: 'course/giant-toolshed-2',
+      imageUrl: 'https://spaceholder.cc/389x292',
+      imageAlt: 'Introduction',
+      progressPercent: 10
+    },
+    {
+      title: 'Giant Toolshed',
+      url: 'course/giant-toolshed-3',
+      imageUrl: 'https://spaceholder.cc/389x292',
+      imageAlt: 'The 5 Gears',
+      progressPercent: 0
+    }
+  ]
+
+  // Render given list of couses as as grid.
+  render() {
+    return (
+      <div className="courses-list">
+        {this.courses.map(course => (
+          <div className="grid-item">
+            <div className="grid-card">
+              <a className="grid-card__cover" href={course.url}><img src={course.imageUrl} alt={course.imageAlt} />
+                <ProgressBar progressPercent={course.progressPercent} />
+              </a>
+              <h3>{course.title}</h3>
+              <div className="grid-card__buttons">
+                <a href={course.url} className="grid-card__secondary-button">Course Home</a>
+                <a href={course.url} className="grid-card__primary-button">Resume</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
+
+// Fetch recent courses and render with standard component.
+class RecentCoursesList extends Component {
+
+  render() {
+    return (
+      <CoursesList />
+    )
+  }
+}
+
+// Dashboard component.
 class StudentDashboard extends Component {
 
   render() {
@@ -7,103 +85,10 @@ class StudentDashboard extends Component {
     return (
       <div className="student-dashboard">
         <h2>Recent Courses</h2>
-        <div className="courses-list">
-          <div className="grid-item">
-            <div className="grid-card">
-              <a className="grid-card__cover" href="#"><img src="https://spaceholder.cc/389x292" alt="" />
-                <div className="progress-bar__small"></div>
-              </a>
-              <h3>The Stars My Destination</h3>
-              <div className="grid-card__buttons">
-                <a href="#" className="grid-card__secondary-button">Course Home</a>
-                <a href="#" className="grid-card__primary-button">Resume</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="grid-item">
-            <div className="grid-card">
-              <a className="grid-card__cover" href="#"><img src="https://spaceholder.cc/389x292" alt="" />
-                <div className="progress-bar__small"></div>
-              </a>
-              <h3>The Stars My Destination</h3>
-              <div className="grid-card__buttons">
-                <a href="#" className="grid-card__secondary-button">Course Home</a>
-                <a href="#" className="grid-card__primary-button">Resume</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="grid-item">
-            <div className="grid-card">
-              <a className="grid-card__cover" href="#"><img src="https://spaceholder.cc/389x292" alt="" />
-                <div className="progress-bar__small"></div>
-              </a>
-              <h3>The Stars My Destination</h3>
-              <div className="grid-card__buttons">
-                <a href="#" className="grid-card__secondary-button">Course Home</a>
-                <a href="#" className="grid-card__primary-button">Resume</a>
-              </div>
-            </div>
-          </div>
-
-        </div>
+        <RecentCoursesList />
 
         <h2>Big Foundations</h2>
-        <div className="courses-list">
-          <div className="grid-item">
-            <div className="grid-card">
-              <a className="grid-card__cover" href="#"><img src="https://spaceholder.cc/389x292" alt="" />
-                <div className="progress-bar__small"></div>
-              </a>
-              <h3>The Stars My Destination</h3>
-              <div className="grid-card__buttons">
-                <a href="#" className="grid-card__secondary-button">Course Home</a>
-                <a href="#" className="grid-card__primary-button">Resume</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="grid-item">
-            <div className="grid-card">
-              <a className="grid-card__cover" href="#"><img src="https://spaceholder.cc/389x292" alt="" />
-                <div className="progress-bar__small"></div>
-              </a>
-              <h3>The Stars My Destination</h3>
-              <div className="grid-card__buttons">
-                <a href="#" className="grid-card__secondary-button">Course Home</a>
-                <a href="#" className="grid-card__primary-button">Resume</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="grid-item">
-            <div className="grid-card">
-              <a className="grid-card__cover" href="#"><img src="https://spaceholder.cc/389x292" alt="" />
-                <div className="progress-bar__small"></div>
-              </a>
-              <h3>The Stars My Destination</h3>
-              <div className="grid-card__buttons">
-                <a href="#" className="grid-card__secondary-button">Course Home</a>
-                <a href="#" className="grid-card__primary-button">Resume</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="grid-item">
-            <div className="grid-card">
-              <a className="grid-card__cover" href="#"><img src="https://spaceholder.cc/389x292" alt="" />
-                <div className="progress-bar__small"></div>
-              </a>
-              <h3>The Stars My Destination</h3>
-              <div className="grid-card__buttons">
-                <a href="#" className="grid-card__secondary-button">Course Home</a>
-                <a href="#" className="grid-card__primary-button">Resume</a>
-              </div>
-            </div>
-          </div>
-
-        </div>
+        <RecentCoursesList />
 
       </div>
     );
