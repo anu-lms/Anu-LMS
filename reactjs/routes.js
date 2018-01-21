53daa20da5fecee1cce944b9ecd431b7a3882e09
spleshka <spleshka@gmail.com>
2018-01-21 12:59:03 +0300

    [#154083397] Added basic data fetch for the course page.

--- reactjs/routes.js
+++ reactjs/routes.js
@@ -3,5 +3,6 @@ const routes = module.exports = require('next-routes')();
 // @see https://github.com/fridays/next-routes
 // Additional dynamic routes.
 routes
+  .add('_course', '/course/:course')
   .add('_lesson', '/course/:course/:lesson');
 
--- reactjs/store/withRedux.js
+++ reactjs/store/withRedux.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/lib/integration/react'
 import { store, persistor } from '../store/store';
-import jsCookie from "js-cookie";
-import request from "../utils/request";
 
 export default function(PageComponent) {
   return class ReduxPage extends React.Component {
