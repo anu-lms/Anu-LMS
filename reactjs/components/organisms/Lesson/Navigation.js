407923bd3867f08764fe7abb9bc6fb676a1b5a02
spleshka <spleshka@gmail.com>
2018-01-21 15:47:34 +0300

    [#154083404] Fixed behavior for mobile devices on navigation click on a lesson page.

--- reactjs/components/organisms/Lesson/Navigation.js
+++ reactjs/components/organisms/Lesson/Navigation.js
@@ -6,40 +6,61 @@ import CollapsibleNavigation from '../../atoms/CollapsibleNavigation';
 import LinkWithProgress from '../../atoms/Link/LinkWithProgress';
 import * as lessonHelper from '../../../helpers/lesson';
 import * as courseHelper from '../../../helpers/course';
+import * as navigationActions from '../../../actions/navigation';
 
-const LessonNavigation = ({ toc, lessons, courses, course, router }) => (
-  <CollapsibleNavigation className="lesson">
+class LessonNavigation extends React.Component {
 
-    <Link to={course.url}>
-      <a className="course-teaser" style={{ backgroundImage: 'url("' + course.imageUrl + '")' }}>
-        <div className="image-overlay" />
-        <h5 className="title">{course.title}</h5>
-        <div className="progress">
-          <div className="completion">{courseHelper.getProgress(courses, course.id)}% complete</div>
-          <div className="progress-bar">
-            <div className="current-progress" style={{ width: courseHelper.getProgress(courses, course.id) + '%' }} />
+  /**
+   * Handle additional click event on links
+   * inside of table of contents.
+   */
+  handleTableOfContentsClick() {
+    // On the mobile device you'd want the navigation to hide
+    // as soon as you click on lesson inside of table of contents.
+    // 768 is a bootstrap md breakpoint.
+    if (window.innerWidth < 768) {
+      this.props.dispatch(navigationActions.toggle());
+    }
+  }
+
+  render() {
+    const { toc, lessons, courses, course, router } = this.props;
+    return (
+      <CollapsibleNavigation className="lesson">
+
+        <Link to={course.url}>
+          <a className="course-teaser" style={{ backgroundImage: 'url("' + course.imageUrl + '")' }}>
+            <div className="image-overlay" />
+            <h5 className="title">{course.title}</h5>
+            <div className="progress">
+              <div className="completion">{courseHelper.getProgress(courses, course.id)}% complete</div>
+              <div className="progress-bar">
+                <div className="current-progress" style={{ width: courseHelper.getProgress(courses, course.id) + '%' }} />
+              </div>
+            </div>
+          </a>
+        </Link>
+
+        <div className="table-of-contents">
+          <h5 className="title">Course Contents</h5>
+          <div className="contents">
+            {toc.map(item => (
+              <LinkWithProgress
+                key={item.id}
+                title={item.title}
+                url={item.url}
+                progress={lessonHelper.getProgress(lessons, item.id)}
+                active={item.url === router.asPath}
+                onClick={this.handleTableOfContentsClick.bind(this)}
+              />
+            ))}
           </div>
         </div>
-      </a>
-    </Link>
 
-    <div className="table-of-contents">
-      <h5 className="title">Course Contents</h5>
-      <div className="contents">
-        {toc.map(item => (
-          <LinkWithProgress
-            key={item.id}
-            title={item.title}
-            url={item.url}
-            progress={lessonHelper.getProgress(lessons, item.id)}
-            active={item.url === router.asPath}
-          />
-        ))}
-      </div>
-    </div>
-
-  </CollapsibleNavigation>
-);
+      </CollapsibleNavigation>
+    );
+  }
+}
 
 const mapStateToProps = ({ lesson, course }) => ({
   lessons: lesson,
