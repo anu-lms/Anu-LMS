0bbf4af45c4a5b5cc4bd8eb25b2b007c3c55db09
kalabro <kalabro@ya.ru>
2018-01-23 10:44:32 +0300

    [#154083397] Added responsive styles.

--- reactjs/components/organisms/Templates/Course/index.js
+++ reactjs/components/organisms/Templates/Course/index.js
@@ -5,39 +5,47 @@ import { Link } from '../../../../routes';
 import * as lessonHelper from "../../../../helpers/lesson";
 import { getProgress } from '../../../../helpers/course';
 
+const ResumeButton = ({ url, progressPercent }) => (
+  <Link to={url}>
+    <a className="btn btn-primary btn-lg btn-block">
+      {progressPercent == 0 && 'Start'}
+      {progressPercent > 0 && 'Resume'}
+    </a>
+  </Link>
+);
+
 const CoursePageTemplate = ({ course, lessons, progressPercent }) => (
   <div className="container container-course">
     <div className="row">
       <div className="col-md-6 course-header">
         <h1>{course.title}</h1>
         <p class="organisation">GiANT Worldwide</p>
-        <p class="instructors">Instructor: <strong>Case Keenum</strong></p>
+        <p class="instructors">Instructor: <span>Case Keenum</span></p>
         <p className="estimated-time">2 hours and 17 minutes remaining</p>
-        <Link to={course.url}>
-          <a className="btn btn-primary btn-lg btn-block">
-            {progressPercent == 0 && 'Start'}
-            {progressPercent > 0 && 'Resume'}
-          </a>
-        </Link>
+        <ResumeButton progressPercent={progressPercent} url={course.url} />
       </div>
-      <div className="col-md-6">
+      <div className="col-md-6 course-cover">
         <img className="course-image" src={course.imageUrl} />
         <div className="progress-bar">
           <div className="current-progress" style={{ width: progressPercent + '%' }} />
         </div>
         <div className="completion">{progressPercent}% complete</div>
+        <ResumeButton progressPercent={progressPercent} url={course.url} />
       </div>
-      <div className="col-md-6">
-        {course.lessons.map(lesson => (
-          <LinkWithProgress
-            key={lesson.id}
-            title={lesson.title}
-            url={lesson.url}
-            progress={lessonHelper.getProgress(lessons, lesson.id)}
-          />
-        ))}
+      <div className="col-md-6 course-lessons">
+        <h3>Course Contents</h3>
+        <div class="lessons-list">
+          {course.lessons.map(lesson => (
+            <LinkWithProgress
+              key={lesson.id}
+              title={lesson.title}
+              url={lesson.url}
+              progress={lessonHelper.getProgress(lessons, lesson.id)}
+            />
+          ))}
+        </div>
       </div>
-      <div className="col-md-6">
+      <div className="col-md-6 course-overview">
         <h3>Overview</h3>
         <p>How to be present and productive when there is never enough time.</p>
       </div>
