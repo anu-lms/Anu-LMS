f2df2776c93641b93028f48b4f47650336c36d95
kalabro <kalabro@ya.ru>
2018-01-30 09:23:36 +0300

    [#154361571] Added Codeception acceptance tests.

---
+++ drupal/tests/codeception/tests/acceptance/frontend/LoginCest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace frontend;
+
+/**
+ * Class LoginCest
+ *
+ * @group frontend
+ */
+class LoginCest {
+
+  public function loginAsManager(\AcceptanceTester $I) {
+    $I->amOnPage('/');
+    $I->fillField('Username', 'moderator.test');
+    $I->fillField('Password', 'password');
+
+    $I->click('Login');
+    $I->waitForText('Recent Courses');
+
+    $I->seeElement('.card');
+  }
+
+}
