922d4419d7bc93af3674c6f163059c8f1a440689
ikorgik <ikorgik@gmail.com>
2018-02-09 11:54:21 +0300

    [#154923105] Added basic notebook data fetcher.

--- reactjs/pages/notebook.js
+++ reactjs/pages/notebook.js
@@ -4,22 +4,42 @@ import withAuth from '../auth/withAuth';
 import withRedux from '../store/withRedux';
 import NotebookTemplate from '../components/organisms/Templates/Notebook';
 import Header from '../components/organisms/Header';
+import * as dataProcessors from '../utils/dataProcessors';
 
 class NotebookPage extends Component {
 
   render() {
+    const { notebook } = this.props;
+    console.log(notebook);
     return (
       <App>
         <Header />
         <div className="page-with-header page-notebook">
-          <NotebookTemplate {...this.props} />
+          <NotebookTemplate course={notebook} />
         </div>
       </App>
     );
   }
 
-  static async getInitialProps({ request }) {
-    const initialProps = {};
+  static async getInitialProps({ request, query, res }) {
+
+    const initialProps = {
+      notebook: {},
+    };
+
+    try {
+      const responseNotebook = await request
+        .get('/jsonapi/notebook/notebook')
+        .query({
+          // Sort by changed date.
+          'sort': 'changed'
+        });
+      initialProps.notebook = dataProcessors.notebookData(responseNotebook.body.data);
+    } catch (error) {
+      if (res) res.statusCode = 404;
+      console.log(error);
+    }
+
     return initialProps;
   }
 }
