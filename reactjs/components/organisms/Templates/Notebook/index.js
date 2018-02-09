93fcc901d8569af4b9c46f6d8b76ea2297c26573
spleshka <spleshka@gmail.com>
2018-02-09 21:35:24 +0300

    [#154926045] Added formatting for notes.

--- reactjs/components/organisms/Templates/Notebook/index.js
+++ reactjs/components/organisms/Templates/Notebook/index.js
@@ -1,16 +1,34 @@
 import React, { Fragment } from'react';
 import NotesList from '../../../moleculas/NotesList';
+import Button from '../../../atoms/Button';
+import Editor from '../../../atoms/RichEditor';
 
 const NotebookTemplate = (props) => (
   <Fragment>
+
     <div className="notes-list-column">
       <NotesList notebook={props.notebook}/>
     </div>
-    <div className="note-content-column d-none d-md-block">
+
+    <div className="note-content d-none d-md-block">
       <div className="container">
         <div className="row">
           <div className="col-12">
-            Hey, I'm a note content
+
+            <div className="caption sm">
+              Updated February 6, 2018 at 5:26pm
+            </div>
+
+            <h4 className="title editable">
+              Outline of an article on Paul Rand’s Thoughts on Design
+            </h4>
+
+            <Editor />
+
+            <div className="mb-5" />
+
+            <Button block className="mt-3">Save</Button>
+
           </div>
         </div>
       </div>
