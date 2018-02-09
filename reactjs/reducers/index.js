b4aa0d957d67e4eaac4302f436ed203f2b5b2fe0
ikorgik <ikorgik@gmail.com>
2018-02-09 20:28:04 +0300

    [#154923105] Added notebook to store.

--- reactjs/reducers/index.js
+++ reactjs/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import course from './course';
 import lesson from './lesson';
+import notebook from './notebook';
 import navigation from './navigation';
 
 export default combineReducers({
   course,
   lesson,
+  notebook,
   navigation
 });
