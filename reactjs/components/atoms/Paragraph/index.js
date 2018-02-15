5c8a40ca87d18e8d099d73c495a7d556ea398f41
Andrei Tyuhai <mailtozap@gmail.com>
2018-02-15 20:48:10 +0300

    [#154792137] Paragraph key should be unique

--- reactjs/components/atoms/Paragraph/index.js
+++ reactjs/components/atoms/Paragraph/index.js
@@ -40,7 +40,7 @@ const Paragraphs = dynamic({
         return (
           <Fragment key={index}>
             <div style={{ display: 'none' }}>{block.id}</div>
-            <Paragraph key={index} {...block} {...props} />
+            <Paragraph key={block.id} {...block} {...props} />
           </Fragment>
         );
       })
