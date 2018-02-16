9674c25beeaf4dac6debc4b18ea2b781d02f6dfa
ikorgik <ikorgik@gmail.com>
2018-02-16 14:35:20 +0300

    [#154562560] Added one column layout.

---
+++ reactjs/components/organisms/Templates/Password/index.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Router } from "../../../../routes";
+
+class PasswordTemplate extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { notes, activeNote, isMobileContentVisible } = this.props;
+
+    return (
+      <Fragment>
+
+
+
+      </Fragment>
+    );
+  }
+}
+
+export default PasswordTemplate;
