8303b0c9a7f0732095d8320de98fc53e070c4a09
ikorgik <ikorgik@gmail.com>
2018-02-16 23:27:22 +0300

    [#154562560] Added mobile and tablet styles for one column layout.

--- reactjs/components/organisms/Templates/OneColumnLayout/index.js
+++ reactjs/components/organisms/Templates/OneColumnLayout/index.js
@@ -10,14 +10,14 @@ class OneColumnLayout extends React.Component {
     return (
       <div className="one-column-layout container">
         {this.props.pageTitle &&
-          <div className="row">
-            <div className="col-12">
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-8 col-lg-12">
               <h1>{this.props.pageTitle}</h1>
             </div>
           </div>
         }
         <div className="row justify-content-center">
-          <div className="col-8">
+          <div className="col-12 col-md-8 col-lg-8">
             {this.props.children}
           </div>
         </div>
