5877314c1700c9dc787241175a5f975206978bf0
ikorgik <ikorgik@gmail.com>
2018-02-21 02:24:47 +0300

    [#154562560] Improved password widget.

--- reactjs/components/atoms/Form/PasswordWidget.js
+++ reactjs/components/atoms/Form/PasswordWidget.js
@@ -3,22 +3,15 @@ import Password from '../FormElement/Password';
 
 const PasswordWidget = (props) => {
   let classes = [];
-  if (props.options.indicator) {
-    classes.push('with-indicator');
-  }
   if (props.options.with_confirm_field) {
     classes.push('with-confirm-field');
   }
   return (
     <Password
       className={classes.join(' ')}
-      minLength={3}
-      minScore={2}
       showIndicator={props.options.indicator}
       changeCallback={(state) => { props.onChange(state.password) }}
-      scoreWords={['Weak', 'Okay', 'Good', 'Strong', 'Very Strong']}
-      tooShortWord='Short'
-      inputProps={{ className: 'form-control', id: props.id, label: props.label, required: props.required, placeholder: props.placeholder }}
+      inputProps={{ id: props.id, label: props.label, required: props.required, placeholder: props.placeholder }}
     />
   );
 }
