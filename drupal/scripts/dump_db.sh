95ebf31e259a29d8c3ebb904461c930e5c368f21
spleshka <spleshka@gmail.com>
2018-02-23 17:46:44 +0300

    [#155286888] Added backup configs to the project.

---
+++ drupal/scripts/dump_db.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Removes older backups from the folder. Keeps last 2 backups on environment.
+function remove_old_sql_dumps() {
+  if [ "$1" ]; then
+
+    # Move to the appropriate backups folder.
+    cd $1
+
+    # List all CNET backups | find total number of backups | decrease by 2.
+    NUMBER_OF_FOLDERS_TO_DEL=$(ls -1q | wc -l | awk '{print $0-2}')
+
+    if [ "$NUMBER_OF_FOLDERS_TO_DEL" -gt 0 ]; then
+        # List all backups | sort by name DESC | print last NUMBER_OF_FOLDERS_TO_DEL backups only | remove them.
+        ls -1q | sort -nr | tail --lines=$NUMBER_OF_FOLDERS_TO_DEL | xargs rm -rfv
+    fi;
+  fi;
+
+}
+
+echo "== DB DUMP STARTED $(date +"%d.%m.%Y %T") =="
+time drush sql-dump -r public --gzip --result-file -v
+
+echo "Removing older dumps..."
+# Grep database name from Drush status output to use it in backup folder.
+DB_NAME=$(drush -r public sql-connect | sed -E "s/.*(--database=(\w+)).*/\\2/")
+
+if [ "DB_NAME" ]; then
+  remove_old_sql_dumps "$HOME/drush-backups/$DB_NAME";
+fi;
+
+echo "== DB DUMP FINISHED $(date +"%d.%m.%Y %T") =="
