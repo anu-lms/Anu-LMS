fb8cc163aee3b214ff51bdeb6b3146b1c75f1d51
ikorgik <ikorgik@gmail.com>
2018-02-28 20:38:07 +0300

    [#155121939] Fixed warning.

--- drupal/tests/codeception/tests/_support/PlatformExtension.php
+++ drupal/tests/codeception/tests/_support/PlatformExtension.php
@@ -28,9 +28,6 @@ class PlatformExtension extends \Codeception\Extension
     codecept_debug($_ENV['BACKEND_URL']);
     codecept_debug($_ENV['FRONTEND_URL']);
 
-    codecept_debug($_ENV['HTTP_USERNAME']);
-    codecept_debug($_ENV['HTTP_PASSWORD']);
-
     // Get URL from CircleCI environment.
     // See circle.yml file to find how this var gets generated.
     $platform_url = $_ENV['BACKEND_URL'];
@@ -43,10 +40,8 @@ class PlatformExtension extends \Codeception\Extension
       $platform_url = $_ENV['FRONTEND_URL'];
     }
 
-
     if (!empty($platform_url)) {
-      $url = rtrim($platform_url, "/");
-      $parsed_url = parse_url($url);
+      $parsed_url = parse_url($platform_url);
       $url = $parsed_url['scheme'] . '://' . $_ENV['HTTP_USERNAME'] . ':' . $_ENV['HTTP_PASSWORD'] . '@' . $parsed_url['host'] . $parsed_url['path'];
 
       if ($this->hasModule('WebDriver')) {
