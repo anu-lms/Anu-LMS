64834c93e46736e16f93926b59f78659ad68c0ba
spleshka <spleshka@gmail.com>
2018-03-06 21:13:58 +0300

    [#155121939] Fixed http auth issues on backend env.

---
+++ drupal/web/modules/anu/anu_auth/src/AnuAuthServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\anu_auth;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+
+/**
+ * Modifies existing services.
+ */
+class AnuAuthServiceProvider extends ServiceProviderBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(ContainerBuilder $container) {
+
+    // Replace Simple Oauth's auth swap class to avoid issues with
+    // http password protected environments when making calls from
+    // the frontend. The class itself does nothing, just takes a place
+    // of the Simple Oauth's class.
+    $definition = $container->getDefinition('simple_oauth.http_middleware.basic_auth_swap');
+    $definition->setClass('Drupal\anu_auth\HttpMiddleware\BasicAuthSwap');
+  }
+}
--- drupal/web/sites/default/http_auth.php
+++ drupal/web/sites/default/http_auth.php
@@ -6,27 +6,24 @@ if (empty($_ENV['HTTP_AUTH_USER']) || empty($_ENV['HTTP_AUTH_PASS'])) {
   return;
 }
 
+// If this is the request from the same host, do not require http auth.
+// The only way to get into the web site is to input http auth credentials,
+// so we remove them for all internal requests within the site.
+$referer = $GLOBALS['request']->server->get('HTTP_REFERER');
+if (!empty($referer)) {
+  $referer_url = parse_url($referer);
+  if ($referer_url['host'] == $GLOBALS['request']->server->get('HTTP_HOST')) {
+    return;
+  }
+}
+
+// Get basic auth credentials from web browser.
 $username = !empty($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : '';
 $password = !empty($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
 
+// Show http auth if user and pass don't match variables in platform.sh.
 if (!($username == $_ENV['HTTP_AUTH_USER'] && $password == $_ENV['HTTP_AUTH_PASS'])) {
   header('WWW-Authenticate: Basic realm="Restricted Page"');
   header('HTTP/1.0 401 Unauthorized');
-  die(http_auth_cancel_page());
-}
-
-/**
- * Returns the page to the unauthenticated user.
- */
-function http_auth_cancel_page() {
-  return
-    '<html>
-      <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Anu LMS | Restricted Site</title>
-      </head>
-      <body class="http-restricted">
-        <p>This page is restricted. Please contact the administrator for access.</p>
-      </body>
-    </html>';
+  die();
 }
