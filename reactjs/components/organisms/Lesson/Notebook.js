4bd076a1d741d3f5b580de02b174abb0abbb9130
ikorgik <ikorgik@gmail.com>
2018-03-15 19:16:43 +0300

    [#155741474] Added shadow.

--- reactjs/components/organisms/Lesson/Notebook.js
+++ reactjs/components/organisms/Lesson/Notebook.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Button from '../../atoms/Button';
 
 const Notebook = () => (
-  <div>
-    <div className="lesson-notebook">
-      This is a notebook!
+  <div className="lesson-notebook">
+    This is a notebook!
 
-      <Button type="link">
-        Save and Close
-      </Button>
-    </div>
+    <Button type="link">
+      Save and Close
+    </Button>
   </div>
 );
 
