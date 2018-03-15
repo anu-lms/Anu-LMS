40978eb683f6dd8e13ead57c47dacdf2af3f7da1
ikorgik <ikorgik@gmail.com>
2018-03-15 19:10:44 +0300

    [#155741474] Added template for notebook.

---
+++ reactjs/components/organisms/Lesson/Notebook.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '../../atoms/Button';
+
+const Notebook = () => (
+  <div>
+    <div className="lesson-notebook">
+      This is a notebook!
+
+      <Button type="link">
+        Save and Close
+      </Button>
+    </div>
+  </div>
+);
+
+export default Notebook;
--- reactjs/components/organisms/Templates/Lesson/index.js
+++ reactjs/components/organisms/Templates/Lesson/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LessonNavigation from '../../../../components/organisms/Lesson/Navigation';
 import LessonContent from '../../../../components/organisms/Lesson/Content';
+import Notebook from '../../../../components/organisms/Lesson/Notebook';
 
 const LessonPageTemplate = ({ course, lesson }) => (
   <div className="pt-3 pt-md-5">
     <LessonNavigation course={course} />
     <LessonContent course={course} lesson={lesson} />
+    <Notebook />
   </div>
 );
 
