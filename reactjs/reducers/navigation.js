fb3700955c42b817ae4d9afaee3b32c1f47b5626
ikorgik <ikorgik@gmail.com>
2018-03-19 14:07:33 +0300

    [#155741474] Fixed an issue with default navigation state on mobile and desktop.

--- reactjs/reducers/navigation.js
+++ reactjs/reducers/navigation.js
@@ -1,19 +1,24 @@
-export default (state = { isCollapsed: true }, action) => {
+// Because we need to hide navigation by default on mobile, but show by default on Desktop,
+// we use isCollapsedMobile property for mobile devices and isCollapsed for desktop.
+export default (state = { isCollapsed: false, isCollapsedMobile: true }, action) => {
 
   switch (action.type) {
     case 'NAVIGATION_TOGGLE':
       return {
         isCollapsed: !state.isCollapsed,
+        isCollapsedMobile: !state.isCollapsed,
       };
 
     case 'NAVIGATION_OPEN':
       return {
         isCollapsed: false,
+        isCollapsedMobile: false,
       };
 
     case 'NAVIGATION_CLOSE':
       return {
         isCollapsed: true,
+        isCollapsedMobile: true,
       };
 
     default:
