93bd19d3efba1a36dc3868fac88ab577aacb414f
spleshka <spleshka@gmail.com>
2018-03-27 20:40:58 +0300

    [#156155214] Added PDF files display.

--- reactjs/utils/breakpoints.js
+++ reactjs/utils/breakpoints.js
@@ -11,18 +11,28 @@ const breakpoints = {
 // @todo: improve functionality, show console warnings if window or breakpoint name missed.
 // Consider other ways instead of `window` variable, because it's not available in all cases.
 export const isUp = (name) => {
-  return window.innerWidth >= breakpoints[name];
+  return getPageWidth() >= breakpoints[name];
 };
 
 export const isDown = (name) => {
-  return window.innerWidth < breakpoints[name];
+  return getPageWidth() < breakpoints[name];
 };
 
 export const isBetween = (lower, upper) => {
-  return window.innerWidth >= breakpoints[lower] && window.innerWidth < breakpoints[upper];
+  return getPageWidth() >= breakpoints[lower] && window.innerWidth < breakpoints[upper];
 };
 
 // @todo: implement behaviour for Only.
 export const isOnly = (name) => {
   return false;
 };
+
+export const getPageWidth = () => (
+  Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  )
+);
