cfe17df4e62a4ec9c07b9b9b530fea82fd60fa77
ikorgik <ikorgik@gmail.com>
2018-04-01 14:01:29 +0300

    Added a comment.

--- reactjs/sagas/index.js
+++ reactjs/sagas/index.js
@@ -3,6 +3,7 @@ import lessonSagas from './lesson';
 import notebookSagas from './notebook';
 import lessonNotebookSagas from './lessonNotebook';
 
+// @todo: consider to use saga plugin for eslint.
 export default function* rootSaga() {
   yield all([
     ...lessonSagas(),
