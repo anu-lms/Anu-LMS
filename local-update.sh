151a9c67b36bfe99d15abc3e891943a58a6bade8
spleshka <spleshka@gmail.com>
2018-04-05 14:01:11 +0300

    Enhanced local env to avoid warning with keys permissions.

--- local-update.sh
+++ local-update.sh
@@ -9,9 +9,9 @@ docker-compose exec --user=82:82 php sh -c "cd web && drush cim -y"
 # Clear all cache
 docker-compose exec --user=82:82 php sh -c "cd web && drush cr"
 
-# Set the right permis for the public key.
-docker-compose exec php sh -c "sudo chmod 660 keys/public.key"
-docker-compose exec php sh -c "sudo chown 82:82 keys/public.key"
+# Set the right permis for the cert keys.
+docker-compose exec php sh -c "sudo chmod 660 keys/public.key keys/private.key"
+docker-compose exec php sh -c "sudo chown 82:82 keys/public.key keys/private.key"
 
 # Install and update all node.js packages.
 docker-compose run node yarn install
