2ea9de11f80914c4179a15e720c38fbe87658157
ikorgik <ikorgik@gmail.com>
2018-04-09 18:49:37 +0300

    [#156259029] Added custom field and filter for node group.

---
+++ drupal/web/modules/anu/anu_content_administration/src/Plugin/views/field/NodeGroups.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @file
+ *
+ */
+
+namespace Drupal\anu_content_administration\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Field handler to show list of user groups.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("anu_node_groups")
+ */
+class NodeGroups extends FieldPluginBase {
+
+  /**
+   * @{inheritdoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $entity = $values->_entity;
+
+    $connection = \Drupal::database();
+    $query = $connection->select('group_content_field_data', 'content');
+    $query->innerJoin('groups_field_data', 'data', 'data.id = content.gid');
+    $query->fields('data', ['id', 'label']);
+    $query->condition('content.entity_id', $entity->id());
+
+    $results = $query->execute();
+
+    $groups = [];
+    foreach ($results as $group) {
+      $edit_link = Link::fromTextAndUrl(
+        $this->t('[edit]'),
+        Url::fromUri('internal:/group/' . $group->id . '/edit', ['query' => \Drupal::destination()->getAsArray()])
+      );
+
+      $groups[] = $group->label . ' ' . $edit_link->toString();
+    }
+
+    return [
+      '#markup' => implode(', ', $groups),
+    ];
+  }
+}
---
+++ drupal/web/modules/anu/anu_content_administration/src/Plugin/views/filter/NodeGroupsFilter.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\anu_content_administration\Plugin\views\filter;
+
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Drupal\views\Plugin\views\filter\InOperator;
+use Drupal\views\ViewExecutable;
+use Drupal\views\Views;
+
+/**
+ * Filters content by groups they belong to.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("anu_node_groups_filter")
+ */
+class NodeGroupsFilter extends InOperator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+    $this->valueTitle = t('Group filter');
+    $this->definition['options callback'] = array($this, 'generateOptions');
+  }
+
+  public function validate() {
+    if (!empty($this->value)) {
+      parent::validate();
+    }
+  }
+
+  public function query() {
+
+    // Join Group's table.
+    $configuration = [
+      'table' => 'group_content_field_data',
+      'field' => 'entity_id',
+      'left_table' => 'node_field_data',
+      'left_field' => 'nid',
+      'type' => 'INNER',
+      'operator' => '=',
+    ];
+
+    $join = Views::pluginManager('join')
+      ->createInstance('standard', $configuration);
+
+    // Filter by users in groups.
+    $this->query->addRelationship('group_content_field_data', $join, 'node');
+    $this->query->addWhere('AND', 'group_content_field_data.gid', $this->value, 'IN');
+  }
+
+  /**
+   * Generate list of groups for filter.
+   */
+  public function generateOptions() {
+    $group_list = [];
+
+    try {
+
+      // Load all available groups.
+      $groups = \Drupal::entityTypeManager()
+        ->getStorage('group')
+        ->loadMultiple();
+
+      // Convert them into an array for forms api.
+      foreach ($groups as $group) {
+        if ($group->access('view')) {
+          $group_list[$group->id()] = $group->label();
+        }
+      }
+
+      // Sort alphabetically by group label.
+      asort($group_list);
+
+    } catch (\Exception $exception) {
+      \Drupal::logger('anu_user')->critical('Could not load list of groups for filter.');
+    }
+
+    return $group_list;
+  }
+}
