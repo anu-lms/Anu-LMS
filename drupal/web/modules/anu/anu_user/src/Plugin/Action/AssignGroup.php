4ad897cd6b5dae3ce8a1d0a86a2105a2989f937f
ikorgik <ikorgik@gmail.com>
2018-04-09 10:34:09 +0300

    [#156259142] Added functionality to add users to several groups.

--- drupal/web/modules/anu/anu_user/src/Plugin/Action/AssignGroup.php
+++ drupal/web/modules/anu/anu_user/src/Plugin/Action/AssignGroup.php
@@ -26,9 +26,13 @@ class AssignGroup extends ViewsBulkOperationsActionBase {
    * {@inheritdoc}
    */
   public function execute($entity = NULL) {
-    $group_id = $this->configuration['setting_group'];
-    $group = \Drupal::entityTypeManager()->getStorage('group')->load($group_id);
-    $group->addContent($entity, 'group_membership');
+    $group_ids = $this->configuration['setting_group'];
+    $groups = \Drupal::entityTypeManager()->getStorage('group')->loadMultiple($group_ids);
+    foreach ($groups as $group) {
+      if (!$group->getMember($entity)) {
+        $group->addContent($entity, 'group_membership');
+      }
+    }
   }
 
   /**
@@ -57,7 +61,9 @@ class AssignGroup extends ViewsBulkOperationsActionBase {
     $form['setting_group'] = [
       '#title' => t('Choose the group name'),
       '#type' => 'select',
+      '#multiple' => TRUE,
       '#options' => $group_list,
+      '#description' => $this->t('Use CTRL/CMD or SHIFT buttons to select several groups.'),
       '#default_value' => $form_state->getValue('setting_group'),
     ];
 
