b81beabd2ab092fa68ba9b22e22b00490ebbff80
ikorgik <ikorgik@gmail.com>
2018-04-09 16:30:34 +0300

    [#156259029] Added functionality to remove courses from classes.

--- drupal/web/modules/anu/anu_content_administration/src/Plugin/Action/RemoveCourseFromClass.php
+++ drupal/web/modules/anu/anu_content_administration/src/Plugin/Action/RemoveCourseFromClass.php
@@ -5,20 +5,21 @@ namespace Drupal\anu_content_administration\Plugin\Action;
 use Drupal\views_bulk_operations\Action\ViewsBulkOperationsActionBase;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\group\Entity\GroupContent;
 
 /**
- * Add a chosen course to classes.
+ * Remove chosen courses from classes.
  *
  * @Action(
- *   id = "anu_add_course_to_class",
- *   label = @Translation("Add Course to Class"),
+ *   id = "anu_remove_course_from_class",
+ *   label = @Translation("Remove chosen courses from classes"),
  *   type = "node",
  *   requirements = {
  *     "_permission" = "administer content",
  *   },
  * )
  */
-class AddCourseToClass extends ViewsBulkOperationsActionBase {
+class RemoveCourseFromClass extends ViewsBulkOperationsActionBase {
 
   use StringTranslationTrait;
 
@@ -26,14 +27,12 @@ class AddCourseToClass extends ViewsBulkOperationsActionBase {
    * {@inheritdoc}
    */
   public function execute($entity = NULL) {
-    $groups = \Drupal::entityTypeManager()
-      ->getStorage('group')
-      ->loadMultiple($this->configuration['classes']);
-
-    /** @var \Drupal\group\Entity\GroupInterface $group */
-    foreach ($groups as $group) {
-      if (!$group->getContentByEntityId('group_node:course', $entity->id())) {
-        $group->addContent($entity, 'group_node:course');
+    if ($group_contents = GroupContent::loadByEntity($entity)) {
+      /** @var \Drupal\group\Entity\GroupContent $group_content */
+      foreach ($group_contents as $group_content) {
+        if (in_array($group_content->getGroup()->id(), $this->configuration['classes'])) {
+          $group_content->delete();
+        }
       }
     }
   }
@@ -41,14 +40,14 @@ class AddCourseToClass extends ViewsBulkOperationsActionBase {
   /**
    * {@inheritdoc}
    */
-  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+  public function access($entity, AccountInterface $account = NULL, $return_as_object = FALSE) {
     $groups = \Drupal::entityTypeManager()
       ->getStorage('group')
       ->loadMultiple($this->configuration['classes']);
 
     /** @var \Drupal\group\Entity\GroupInterface $group */
     foreach ($groups as $group) {
-      if (!$group->access('update')) {
+      if (!$group->access('delete')) {
         return FALSE;
       }
     }
@@ -64,18 +63,20 @@ class AddCourseToClass extends ViewsBulkOperationsActionBase {
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    */
   public function buildConfigurationForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    $groups = \Drupal::entityTypeManager()->getStorage('group')->loadMultiple();
+    $groups = \Drupal::entityTypeManager()
+      ->getStorage('group')
+      ->loadMultiple();
 
     $group_list = [];
     /** @var \Drupal\group\Entity\GroupInterface $group */
     foreach ($groups as $group) {
-      if ($group->access('update')) {
+      if ($group->access('delete')) {
         $group_list[$group->id()] = $group->label();
       }
     }
 
     $form['classes'] = [
-      '#title' => t('Choose the Classes to assign to the selected courses'),
+      '#title' => t('Choose the Classes to remove from the selected courses:'),
       '#type' => 'checkboxes',
       '#options' => $group_list,
     ];
