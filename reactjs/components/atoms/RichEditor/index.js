357041596e73da26af8595906098f86aef093e27
ikorgik <ikorgik@gmail.com>
2018-04-12 15:56:10 +0300

    [#156505745] Added comment.

--- reactjs/components/atoms/RichEditor/index.js
+++ reactjs/components/atoms/RichEditor/index.js
@@ -14,6 +14,7 @@ import '../../../utils/polyfill/closest';
 
 import { html } from './serializer';
 
+// Enable EditList plugin to handle keyboard events in lists.
 const pluginEditList = EditList({
   types: ['bulleted-list', 'numbered-list'],
   typeItem: 'list-item',
