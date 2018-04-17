c0d12e4a30db796c81c6fa2b0fc3812f3b415d6f
ikorgik <ikorgik@gmail.com>
2018-04-17 13:35:58 +0300

    [#156129214] Fixed indentation.

--- drupal/web/modules/anu/anu_test_content/src/EventSubscriber/ImportEventSubscriber.php
+++ drupal/web/modules/anu/anu_test_content/src/EventSubscriber/ImportEventSubscriber.php
@@ -7,7 +7,6 @@
 
 namespace Drupal\anu_test_content\EventSubscriber;
 
-
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\default_content\Event\ImportEvent;
 
@@ -17,86 +16,86 @@ use Drupal\default_content\Event\ImportEvent;
  */
 class ImportEventSubscriber implements EventSubscriberInterface {
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function getSubscribedEvents() {
-        $events['default_content.import'][] = array('onDefaultContentImport');
-        return $events;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events['default_content.import'][] = array('onDefaultContentImport');
+    return $events;
+  }
 
-    /**
-     * Subscriber Callback for the event.
-     * @param ImportEvent $event
-     */
-    public function onDefaultContentImport(ImportEvent $event) {
+  /**
+   * Subscriber Callback for the event.
+   * @param ImportEvent $event
+   */
+  public function onDefaultContentImport(ImportEvent $event) {
 
-        if ($entities = $event->getImportedEntities()) {
-            // Get imported Classes.
-            $groups = array_filter($entities, function($entity) {
-                /* @var $entity \Drupal\Core\Entity\ContentEntityInterface */
-                return $entity->getEntityTypeId() === 'group' ? true : false;
-            });
-            // Get imported Courses and Lessons.
-            $nodes = array_filter($entities, function($entity) {
-                /* @var $entity \Drupal\Core\Entity\ContentEntityInterface */
-                return $entity->getEntityTypeId() === 'node' ? true : false;
-            });
+    if ($entities = $event->getImportedEntities()) {
+      // Get imported Classes.
+      $groups = array_filter($entities, function($entity) {
 
-            foreach ($groups as $group) {
-                /* @var $group \Drupal\group\Entity\Group*/
-                $this->addTestClassMembers($group);
-                // Add all test courses and lessons into all test classes.
-                /* @var $node \Drupal\node\Entity\Node*/
-                foreach ($nodes as $node) {
-                    $bundle = $node->bundle();
-                    if (in_array($bundle, ['course', 'lesson'])) {
-                        $plugin = 'group_node:' . $node->bundle();
-                        $group->addContent($node, $plugin);
-                    }
+        /* @var $entity \Drupal\Core\Entity\ContentEntityInterface */
+        return $entity->getEntityTypeId() === 'group' ? true : false;
+      });
 
-                }
-            }
+      // Get imported Courses and Lessons.
+      $nodes = array_filter($entities, function($entity) {
+        /* @var $entity \Drupal\Core\Entity\ContentEntityInterface */
+        return $entity->getEntityTypeId() === 'node' ? true : false;
+      });
+
+      foreach ($groups as $group) {
+        /* @var $group \Drupal\group\Entity\Group */
+        $this->addTestClassMembers($group);
+
+        // Add all test courses and lessons into all test classes.
+        /* @var $node \Drupal\node\Entity\Node */
+        foreach ($nodes as $node) {
+          $bundle = $node->bundle();
+          if (in_array($bundle, ['course', 'lesson'])) {
+            $plugin = 'group_node:' . $node->bundle();
+            $group->addContent($node, $plugin);
+          }
         }
-
+      }
     }
+  }
 
+  /**
+   * Add users into the test group.
+   * @param $group \Drupal\group\Entity\Group
+   */
+  public function addTestClassMembers($group) {
 
-    /**
-     * Add users into the test group.
-     * @param $group \Drupal\group\Entity\Group
-     */
-    public function addTestClassMembers($group) {
+    /* @var $role \Drupal\user\Entity\Role */
+    foreach (\Drupal\user\Entity\Role::loadMultiple() as $role) {
 
-        /* @var $role \Drupal\user\Entity\Role */
-        foreach (\Drupal\user\Entity\Role::loadMultiple() as $role) {
+      if ($role->id() == 'anonymous') {
+        continue;
+      }
 
-            if ($role->id() == 'anonymous') {
-                continue;
-            }
+      switch ($role->id()) {
+        case 'teacher':
+        case 'manager':
+        case 'admin':
+          $roles = ['class-admin'];
+          break;
+        default:
+          $roles = [];
+          break;
+      }
 
-            switch ($role->id()) {
-                case 'teacher':
-                case 'manager':
-                case 'admin':
-                    $roles = ['class-admin'];
-                    break;
-                default:
-                    $roles = [];
-                    break;
-            }
+      $values = ['group_roles' => $roles, 'gid' => $group->id()];
 
-            $values = ['group_roles' => $roles, 'gid' => $group->id()];
+      $username = $role->id() . '.test';
 
-            $username = $role->id() . '.test';
+      /* @var $account \Drupal\user\Entity\User */
+      $account = user_load_by_name($username);
 
-            /* @var $account \Drupal\user\Entity\User */
-            $account = user_load_by_name($username);
-
-            if ($account) {
-                $group->addMember($account, $values);
-            }
-        }
+      if ($account) {
+        $group->addMember($account, $values);
+      }
     }
+  }
 
 }
