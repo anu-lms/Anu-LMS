b453aad9e02bb78efbdb4362e0a64b0095bc0efb
ikorgik <ikorgik@gmail.com>
2018-04-19 14:33:04 +0300

    [#155219435] Added parent property for Sticky formating.

--- reactjs/components/atoms/Sticky/index.js
+++ reactjs/components/atoms/Sticky/index.js
@@ -1,51 +1,72 @@
+import Debug from 'debug';
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const debug = Debug('anu:sticky');
+
 /**
- * Based on https://github.com/themeteorchef/react-sticky-scroll/blob/master/imports/ui/components/sticky.js
- * with bug fixes in row:12-13.
+ * Based on https://github.com/themeteorchef/react-sticky-scroll/blob/master/imports/ui/components/sticky.js.
  */
 class Sticky extends React.Component {
   componentDidMount() {
-    const setInitialHeights = (elements) => {
-      [].forEach.call(elements, (sticky) => {
-        const top = document.documentElement.scrollTop || document.body.scrollTop;
-        const elementTop = sticky.getBoundingClientRect().top;
-        sticky.setAttribute('data-sticky-initial', top + elementTop);
-      });
-    };
+    const parentElement = this.props.rootId ? document.getElementById(this.props.rootId) : document.documentElement || document.body;
+    debug('componentDidMount:parentElement', parentElement);
 
     const stickies = document.querySelectorAll('[data-sticky]');
-    setInitialHeights(stickies);
+    debug('componentDidMount:stickies', stickies);
 
-    document.addEventListener('scroll', () => {
-      const top = document.documentElement.scrollTop || document.body.scrollTop;
-      const bottom = document.documentElement.scrollHeight || document.body.scrollHeight;
+    this.setInitialHeights(parentElement, stickies);
 
-      [].forEach.call(stickies, (sticky) => {
-        const stickyInitial = parseInt(sticky.getAttribute('data-sticky-initial'), 10);
-        const stickyEnter = parseInt(sticky.getAttribute('data-sticky-enter'), 10) || stickyInitial;
-        const stickyExit = parseInt(sticky.getAttribute('data-sticky-exit'), 10) || bottom;
+    const rootElement = this.props.rootId ? document.getElementById(this.props.rootId) : document;
+    this.onScrollHandlerId = () => { this.onScroll(parentElement, stickies) };
+    rootElement.addEventListener('scroll', this.onScrollHandlerId);
+  }
 
-        if (top >= stickyEnter && top <= stickyExit) {
-          sticky.classList.add('sticky');
-        } else {
-          sticky.classList.remove('sticky');
-        }
-      });
+  componentWillUnmount() {
+    const rootElement = this.props.rootId ? document.getElementById(this.props.rootId) : document;
+    rootElement.removeEventListener('scroll', this.onScrollHandlerId);
+  }
+
+  setInitialHeights = (parentElement, elements) => {
+    [].forEach.call(elements, (sticky) => {
+
+      const top = parentElement.scrollTop;
+      const elementTop = sticky.getBoundingClientRect().top;
+      sticky.setAttribute('data-sticky-initial', top + elementTop);
+      debug('setInitialHeights', { top, elementTop, stickyInitial: top + elementTop });
+    });
+  };
+
+  onScroll(parentElement, stickies) {
+    const top = parentElement.scrollTop;
+    const bottom = parentElement.scrollHeight;
+
+    [].forEach.call(stickies, (sticky) => {
+      const stickyInitial = parseInt(sticky.getAttribute('data-sticky-initial'), 10);
+      const stickyEnter = parseInt(sticky.getAttribute('data-sticky-enter'), 10) || stickyInitial;
+      const stickyExit = parseInt(sticky.getAttribute('data-sticky-exit'), 10) || bottom;
+
+      if (top >= stickyEnter && top <= stickyExit) {
+        sticky.classList.add('sticky');
+      } else {
+        sticky.classList.remove('sticky');
+      }
+      debug('scroll', { top, bottom, stickyEnter, stickyExit });
     });
   }
 
   render() {
     const { className, enter, exit, children } = this.props;
-    return (<div
-      className={`Sticky ${className}`}
-      data-sticky
-      data-sticky-enter={enter}
-      data-sticky-exit={exit}
-    >
-      {children}
-    </div>);
+    return (
+      <div
+        className={`sticky-element ${className}`}
+        data-sticky
+        data-sticky-enter={enter}
+        data-sticky-exit={exit}
+      >
+        {children}
+      </div>
+    );
   }
 }
 
@@ -54,6 +75,7 @@ Sticky.propTypes = {
   enter: PropTypes.string,
   exit: PropTypes.string,
   children: PropTypes.node,
+  parentId: PropTypes.string,
 };
 
 export default Sticky;
