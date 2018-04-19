f053e6fea6c51c1cb23d3ad6ad180036bfafdb20
ikorgik <ikorgik@gmail.com>
2018-04-19 23:25:49 +0300

    [#156925737] Improved gtm test.

--- tests/codeception/tests/acceptance/frontend/FrontPageCest.php
+++ tests/codeception/tests/acceptance/frontend/FrontPageCest.php
@@ -10,11 +10,12 @@ namespace frontend;
 class FrontPageCest {
 
   public function testGTM(\Step\Acceptance\Learner $I) {
+    if (empty($_ENV['GTM_ID_DEV'])) {
+      throw new \Exception("GTM_ID_DEV variable doesn't exists, make sure you've added it in .env.local file and in CI variables");
+    }
+    $GTM = $_ENV['GTM_ID_DEV'];
 
     $I->amOnPage('/');
-    $GTM = $_ENV['GTM_ID_DEV'];
-    codecept_debug($_ENV);
-
     $I->expectTo('see GTM container on site in stage mode.');
     $I->seeInPageSource("id=$GTM");
     $I->seeInPageSource("gtm_preview=env-5");
