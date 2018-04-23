c453ab246f656b7db1b212222566e830fdee921d
ikorgik <ikorgik@gmail.com>
2018-04-23 20:13:07 +0300

    [#156770078] Improved comment component.

--- reactjs/components/atoms/Comment/index.js
+++ reactjs/components/atoms/Comment/index.js
@@ -5,26 +5,31 @@ const Comment = ({ comment }) => (
   <div className="">
     <div>id: {comment.id}</div>
     <div>author: {comment.author.name}</div>
-    <div>parent: {comment.parent}</div>
+    <div>parent: {comment.parent || 'no parrent'}</div>
     <div>{comment.text}</div>
   </div>
 );
 
 Comment.propTypes = {
   comment: PropTypes.shape({
-    id: PropTypes.number,
+    id: PropTypes.number.isRequired,
     parent: PropTypes.number,
-    changed: PropTypes.number,
-    created: PropTypes.number,
+    changed: PropTypes.number.isRequired,
+    created: PropTypes.number.isRequired,
     text: PropTypes.string,
-    uuid: PropTypes.string,
+    uuid: PropTypes.string.isRequired,
     author: PropTypes.shape({
-      uid: PropTypes.number,
-      name: PropTypes.string,
+      uid: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
       firstName: PropTypes.string,
       lastName: PropTypes.string,
     }),
   }).isRequired,
+};
+
+Comment.defaultProps = {
+  parent: null,
+  text: '',
 };
 
 export default Comment;
