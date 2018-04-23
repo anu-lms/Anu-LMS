ed86cdb9138d1aeba01a6ac2085a6341f32e2aee
ikorgik <ikorgik@gmail.com>
2018-04-23 20:52:27 +0300

    [#156654068] Renamed component.

--- reactjs/components/organisms/Templates/Lesson/index.js
+++ reactjs/components/organisms/Templates/Lesson/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LessonNavigation from '../../../../components/organisms/Lesson/Navigation';
 import LessonContent from '../../../../components/organisms/Lesson/Content';
-import Sidebar from '../../../../components/organisms/Lesson/Sidebar';
+import LessonSidebar from '../../../../components/organisms/Lesson/Sidebar';
 
 const LessonPageTemplate = ({ course, lesson }) => (
   <div className="pt-3 pt-md-5">
     <LessonNavigation course={course} />
     <LessonContent course={course} lesson={lesson} />
-    <Sidebar />
+    <LessonSidebar />
   </div>
 );
 
