c46681debdc18934e89e3aa78943857794ed672b
spleshka <spleshka@gmail.com>
2018-04-25 14:49:52 +0300

    [#156654121] Added styling for the comments.

--- reactjs/components/atoms/Comment/index.js
+++ reactjs/components/atoms/Comment/index.js
@@ -1,10 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import * as userHelper from '../../../helpers/user';
 
-const Comment = ({ comment }) => (
-  <div className={`comments-item ${comment.parent ? 'nested' : ''}`}>
-    <div>{comment.author.name} {comment.parent ? ` > ${comment.parent.author.name}` : ''}</div>
-    <div>{comment.text}</div>
+const Comment = ({comment}) => (
+  <div className={`comment ${comment.parent ? 'nested' : ''}`}>
+
+    <div className="comment-header">
+      <div className="avatar" style={{ background: userHelper.getUserColor(comment.author) }}>
+        {userHelper.getInitials(comment.author)}
+      </div>
+      <div className="right">
+
+        <div className="username">
+          {userHelper.getUsername(comment.author)}
+          {comment.parent &&
+          <span className="replied-to">
+            &nbsp;>&nbsp;{userHelper.getUsername(comment.parent.author)}
+          </span>
+          }
+        </div>
+
+        <div className="date">
+          <Moment parse="unix" format="MMM Do, YYYY">{comment.created * 1000}</Moment>
+        </div>
+
+      </div>
+    </div>
+
+    <div className="comment-body">
+      <div dangerouslySetInnerHTML={{__html: comment.text}}/>
+    </div>
+
+    <div className="comment-footer">
+      <div className="links">
+
+        <span className="link reply">
+          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11"
+               viewBox="0 0 12 11">
+            <g fill="none" fillRule="evenodd">
+              <path fill="#4A4A4A" fillRule="nonzero"
+                    d="M4.667 3V.333L0 5l4.667 4.667V6.933C8 6.933 10.333 8 12 10.333 11.333 7 9.333 3.667 4.667 3z"/>
+            </g>
+          </svg>
+          <span className="label">reply</span>
+        </span>
+
+      </div>
+    </div>
+
   </div>
 );
 
