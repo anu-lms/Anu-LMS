af7e58248e5442b61f4e9dcd4a1c263142621d36
ikorgik <ikorgik@gmail.com>
2018-04-27 13:58:05 +0300

    [#156653939] Added validation for paragraph_comment.

---
+++ drupal/web/modules/anu/anu_comments/src/Plugin/Validation/Constraint/ParagraphCommentConstraint.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\anu_comments\Plugin\Validation\Constraint;
+
+use Drupal\Core\Entity\Plugin\Validation\Constraint\CompositeConstraintBase;
+
+/**
+ * Additional validation for paragraph_comment entity.
+ *
+ * @Constraint(
+ *   id = "ParagraphComment",
+ *   label = @Translation("Additional validation for paragraph_comment entity", context = "Validation"),
+ *   type = "entity:paragraph_comment"
+ * )
+ */
+class ParagraphCommentConstraint extends CompositeConstraintBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function coversFields() {
+    return ['field_comment_parent', 'field_comment_paragraph'];
+  }
+}
