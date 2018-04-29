8222225a869c2d30c2fce65273c94d9f8942d3c3
ikorgik <ikorgik@gmail.com>
2018-04-29 20:45:49 +0300

    [#156654263] Added an example of ReactCSSTransitionGroup.

--- reactjs/components/atoms/Comment/List/index.js
+++ reactjs/components/atoms/Comment/List/index.js
@@ -1,46 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import Comment from '../Item';
 import AddCommentForm from '../Form';
 import * as userHelper from '../../../../helpers/user';
 
-const CommentsList = ({ comments, replyTo }) => {
-  const flatCommentsList = [];
 
-  // Prepare list of comments.
-  comments.forEach(rootComment => {
-    let replyToComment = null;
-    if (replyTo === rootComment.id) {
-      replyToComment = rootComment;
-    }
+class CommentsList extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
 
-    // Add root comment to the list.
-    flatCommentsList.push(<Comment comment={rootComment} key={rootComment.id} />);
+  render() {
+    const { comments, replyTo } = this.props;
+    const flatCommentsList = [];
 
-    // Add all children to the list below root component.
-    rootComment.children.forEach(comment => {
-      flatCommentsList.push(<Comment comment={comment} key={comment.id} />);
+    // Prepare list of comments.
+    comments.forEach(rootComment => {
+      let replyToComment = null;
+      if (replyTo === rootComment.id) {
+        replyToComment = rootComment;
+      }
 
-      if (replyTo === comment.id) {
-        replyToComment = comment;
+      // Add root comment to the list.
+      flatCommentsList.push(<Comment comment={rootComment} key={rootComment.id} />);
+
+      // Add all children to the list below root component.
+      rootComment.children.forEach(comment => {
+        flatCommentsList.push(<Comment comment={comment} key={comment.id} />);
+
+        if (replyTo === comment.id) {
+          replyToComment = comment;
+        }
+      });
+
+      // Shows Reply to form at the bottom of root component thread.
+      if (replyToComment) {
+        const placeholder = `Reply to ${userHelper.getUsername(replyToComment.author)}`;
+
+        flatCommentsList.push(<AddCommentForm className="nested" id="reply-comment-form" placeholder={placeholder} key={`${rootComment.id}-form`} />);
       }
     });
 
-    // Shows Reply to form at the bottom of root component thread.
-    if (replyToComment) {
-      const placeholder = `Reply to ${userHelper.getUsername(replyToComment.author)}`;
+    return (
+      <div className="comments-list">
 
-      flatCommentsList.push(<AddCommentForm className="nested" id="reply-comment-form" placeholder={placeholder} key={`${rootComment.id}-form`} />);
-    }
-  });
+        <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}
+        >
 
-  return (
-    <div className="comments-list">
-      {flatCommentsList}
-    </div>
-  );
-};
+          {flatCommentsList}
+        </ReactCSSTransitionGroup>
+      </div>
+    );
+  }
+}
 
 CommentsList.propTypes = {
   comments: PropTypes.arrayOf(PropTypes.object).isRequired,
