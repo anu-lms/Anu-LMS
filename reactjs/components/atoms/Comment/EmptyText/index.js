f9e934e26f1ae3de6784cbff069e1b2ecaf7a018
spleshka <spleshka@gmail.com>
2018-05-01 18:27:42 +0300

    [#156539650]Fixed eslint warnings.

--- reactjs/components/atoms/Comment/EmptyText/index.js
+++ reactjs/components/atoms/Comment/EmptyText/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const EmptyText = () => (
   <div className="empty-text">
-    It's quiet. A little <em>too</em> quite. <br /><br />
+    It&apos;s quiet. A little <em>too</em> quite. <br /><br />
     <strong>Be the first to comment!</strong>
   </div>
 );
