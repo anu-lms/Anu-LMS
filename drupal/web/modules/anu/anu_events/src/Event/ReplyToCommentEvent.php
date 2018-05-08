5862ad369394908a590a5471c0cdf75c8444bf86
ikorgik <ikorgik@gmail.com>
2018-05-08 13:16:32 +0300

    [#157049100] Improved ReplyToCommentEvent class.

--- drupal/web/modules/anu/anu_events/src/Event/ReplyToCommentEvent.php
+++ drupal/web/modules/anu/anu_events/src/Event/ReplyToCommentEvent.php
@@ -2,16 +2,28 @@
 
 namespace Drupal\anu_events\Event;
 
+use \Drupal\Component\Render\FormattableMarkup;
+
 class ReplyToCommentEvent extends AnuEvent {
 
   public function __construct($entity, $template_name = '') {
     parent::__construct($entity, AnuEvents::REPLY_TO_COMMENT,'reply_to_comment');
-    $this->recipient = (int) $this->entity->field_comment_parent
-      ->first()
-      ->get('entity')
-      ->getValue()
-      ->uid
-      ->target_id;
+
+    try {
+      $this->recipient = (int) $this->entity->field_comment_parent
+        ->first()
+        ->get('entity')
+        ->getValue()
+        ->uid
+        ->target_id;
+    }
+    catch (\Exception $exception) {
+      $message = new FormattableMarkup('Could not set notification recipient for comment @id. Error: @error', [
+        '@id' => $this->entity->id(),
+        '@error' => $exception->getMessage()
+      ]);
+      \Drupal::logger('anu_events')->error($message);
+    }
   }
 
   public function attachMessageFields($message) {
