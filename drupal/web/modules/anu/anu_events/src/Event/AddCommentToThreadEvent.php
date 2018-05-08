6a946c3a7a9534867604235a3c65ca4e5cd0c816
ikorgik <ikorgik@gmail.com>
2018-05-08 15:07:01 +0300

    [#157049100] Commited test find root function

--- drupal/web/modules/anu/anu_events/src/Event/AddCommentToThreadEvent.php
+++ drupal/web/modules/anu/anu_events/src/Event/AddCommentToThreadEvent.php
@@ -19,4 +19,41 @@ class AddCommentToThreadEvent extends AnuEvent {
     $message->field_message_comment = $comment->id();
     $message->field_message_recipient = $this->getRecipient();
   }
+
+  private function findRootComment() {
+    $comment = $this->getEntity();
+
+    // Load all comments from the paragraph and organization.
+    $query = \Drupal::entityQuery('paragraph_comment');
+    $query->condition('type', 'paragraph_comment');
+
+    $value = $comment->field_comment_paragraph->getValue();
+    $query->condition('field_comment_paragraph', $value[0]['value']);
+
+    $value = $comment->field_comment_organization->getValue();
+    if (!empty($value[0]['target_id'])) {
+      $query->condition('field_comment_organization', $value[0]['target_id']);
+    }
+    else {
+      $query->notExists('field_comment_organization');
+    }
+
+    $entity_ids = $query->execute();
+    $comments = \Drupal::entityTypeManager()
+      ->getStorage('paragraph_comment')
+      ->loadMultiple($entity_ids);
+
+    if (!empty($comment->field_comment_parent->getValue())) {
+      $parent_id = $comment->field_comment_parent->getString();
+
+      $current_comment = $comments[$parent_id];
+      if (!empty($current_comment->field_comment_parent->getValue())) {
+        $parent_id = $current_comment->field_comment_parent->getString();
+      }
+      else {
+        return $current_comment->id();
+      }
+    }
+  }
+
 }
