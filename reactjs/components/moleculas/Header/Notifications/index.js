847accf32707908b356d75e7320debefc5d2a19b
ikorgik <ikorgik@gmail.com>
2018-05-09 12:28:40 +0300

    [#157048528] Added basic styles for notifications popup.

--- reactjs/components/moleculas/Header/Notifications/index.js
+++ reactjs/components/moleculas/Header/Notifications/index.js
@@ -16,7 +16,7 @@ class Notifications extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="notifications-wrapper">
         <div className="icon-wrapper">
 
           <div className="icon">
@@ -26,10 +26,62 @@ class Notifications extends React.Component {
                 <path d="M-6-3h40v40H-6z" />
               </g>
             </svg>
-            <div className="icon-amount">2</div>
+            {/* <div className="icon-amount">2</div> */}
           </div>
 
-          <div className="icon-label">Notifications</div>
+          {/* <div className="icon-label">Notifications</div> */}
+        </div>
+
+        <div className="notifications-popup">
+          <div className="list">
+
+            <div className="item not-read">
+              <div className="header">
+                <div className="type-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                    <g fill="none" fillRule="evenodd">
+                      <path fill="#B2B2B2" fillRule="nonzero" d="M19.99 2c0-1.1-.89-2-1.99-2H2C.9 0 0 .9 0 2v12c0 1.1.9 2 2 2h14l4 4-.01-18z" />
+                      <path d="M-2-2h24v24H-2z" />
+                    </g>
+                  </svg>
+                </div>
+                <div className="date">
+                  May 7th
+                </div>
+              </div>
+              <div className="title">
+                <b>Hannah Burton</b> replied to your comment in <b>Module 3: Carbohydrates</b>
+              </div>
+              <div className="body">
+                “Body of notification, text falls into this box with 16pt padding right, 32pt padding left. Does the imbalance look odd? I don’t think so.”
+              </div>
+            </div>
+            <div className="item">
+              <div className="header">
+                <div className="type-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                    <g fill="none" fillRule="evenodd">
+                      <path fill="#B2B2B2" fillRule="nonzero" d="M19.99 2c0-1.1-.89-2-1.99-2H2C.9 0 0 .9 0 2v12c0 1.1.9 2 2 2h14l4 4-.01-18z" />
+                      <path d="M-2-2h24v24H-2z" />
+                    </g>
+                  </svg>
+                </div>
+                <div className="date">
+                  May 7th
+                </div>
+              </div>
+              <div className="title">
+                <b>Hannah Burton</b> replied to your comment in <b>Module 3: Carbohydrates</b>
+              </div>
+              <div className="body">
+                “Body of notification, text falls into this box with 16pt padding right, 32pt padding left. Does the imbalance look odd? I don’t think so.”
+              </div>
+            </div>
+
+          </div>
+          <div className="footer">
+            Mark all as read
+          </div>
         </div>
       </div>
     );
