7966232fe2784069e5f644ae551648fb43deb883
ikorgik <ikorgik@gmail.com>
2018-05-10 10:11:18 +0300

    [#157048528] Added open/close function.

--- reactjs/components/moleculas/Header/Notifications/index.js
+++ reactjs/components/moleculas/Header/Notifications/index.js
@@ -7,6 +7,12 @@ import NotificationsPopup from '../../../atoms/Notifications/Popup';
 class Notifications extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isOpen: false,
+    };
+
+    this.closePopup = this.closePopup.bind(this);
+    this.togglePopup = this.togglePopup.bind(this);
   }
 
   componentDidMount() {
@@ -15,12 +21,20 @@ class Notifications extends React.Component {
     dispatch(notificationsActions.syncNotifications());
   }
 
+  closePopup() {
+    this.setState({ isOpen: false });
+  }
+
+  togglePopup() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
   render() {
     return (
       <div className="notifications-wrapper">
         <div className="icon-wrapper">
 
-          <div className="icon">
+          <div className="icon" onClick={this.togglePopup}>
             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="33" viewBox="0 0 28 33">
               <g fill="none" fillRule="evenodd">
                 <path fill="#FFF" fillRule="nonzero" d="M14 32.833c1.833 0 3.333-1.5 3.333-3.333h-6.666A3.332 3.332 0 0 0 14 32.833zm10-10V14.5c0-5.117-2.733-9.4-7.5-10.533V2.833c0-1.383-1.117-2.5-2.5-2.5a2.497 2.497 0 0 0-2.5 2.5v1.134C6.717 5.1 4 9.367 4 14.5v8.333L.667 26.167v1.666h26.666v-1.666L24 22.833z" />
@@ -33,7 +47,9 @@ class Notifications extends React.Component {
           {/* <div className="icon-label">Notifications</div> */}
         </div>
 
-        <NotificationsPopup />
+        {this.state.isOpen &&
+          <NotificationsPopup onCloseClick={this.closePopup} />
+        }
       </div>
     );
   }
