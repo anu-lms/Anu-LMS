16dc689fa35ad677f632b58d7219079e783a5001
ikorgik <ikorgik@gmail.com>
2018-05-10 20:03:30 +0300

    [#157048528] Improved Notifications component.ˆ

--- reactjs/components/atoms/Notifications/Popup/index.js
+++ reactjs/components/atoms/Notifications/Popup/index.js
@@ -3,64 +3,49 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Scrollbars } from 'react-custom-scrollbars';
 import NotificationCommentItem, { supportedBundles as commentSupportedBundles } from '../CommentItem';
-import * as notificationsActions from '../../../../actions/notifications';
 
-class NotificationsPopup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.closePopup = this.closePopup.bind(this);
-  }
+const NotificationsPopup = ({ notifications, isOpened, onCloseClick, onMarkAllAsReadClick }) => (
+  <Fragment>
+    <div className={`notifications-popup ${isOpened ? 'opened' : 'closed'}`}>
 
-  componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch(notificationsActions.syncNotifications());
-  }
+      <div className="list">
+        <Scrollbars style={{ height: '100%' }}>
+          {notifications.map(item => {
+            if (commentSupportedBundles.indexOf(item.bundle) >= 0) {
+              return <NotificationCommentItem notificationItem={item} key={item.id} />;
+            }
+            return null;
+          })}
+        </Scrollbars>
+      </div>
 
-  closePopup() {
-    const { dispatch } = this.props;
-    dispatch(notificationsActions.closePopup());
-  }
+      <div className="footer">
+        <div className="button mark-as-read" onClick={onMarkAllAsReadClick}>Mark all as read</div>
+        <div className="button close" onClick={onCloseClick}>Close Notifications</div>
+      </div>
 
-  render() {
-    const { notifications, isOpened } = this.props;
+    </div>
 
-    return (
-      <Fragment>
-        <div className={`notifications-popup ${isOpened ? 'opened' : 'closed'}`}>
-          <div className="list">
-            <Scrollbars style={{ height: '100%' }}>
-              {notifications.map(item => {
-              if (commentSupportedBundles.indexOf(item.bundle) >= 0) {
-                return <NotificationCommentItem notificationItem={item} key={item.id} />;
-              }
-              return null;
-            })}
-            </Scrollbars>
-          </div>
-          <div className="footer">
-            <div className="button mark-as-read">Mark all as read</div>
-            <div className="button close" onClick={this.closePopup}>Close Notifications</div>
-          </div>
-        </div>
-        {isOpened &&
-        <div className="scrim" onClick={this.closePopup} />
-        }
-      </Fragment>
-    );
-  }
-}
+    {isOpened &&
+    <div className="scrim" onClick={onCloseClick} />
+    }
+  </Fragment>
+);
 
 NotificationsPopup.propTypes = {
   dispatch: PropTypes.func.isRequired,
+  isOpened: PropTypes.bool.isRequired,
+  onCloseClick: PropTypes.func,
+  onMarkAllAsReadClick: PropTypes.func,
 };
 
 NotificationsPopup.defaultProps = {
-
+  onCloseClick: () => {},
+  onMarkAllAsReadClick: () => {},
 };
 
 const mapStateToProps = ({ notifications }) => ({
   notifications: notifications.notifications,
-  isOpened: notifications.isOpened,
 });
 
 export default connect(mapStateToProps)(NotificationsPopup);
