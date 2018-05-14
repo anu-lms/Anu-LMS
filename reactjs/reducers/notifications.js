8730961279e63c4a95b0ceccb42c4df9cec81ba5
ikorgik <ikorgik@gmail.com>
2018-05-14 14:23:10 +0300

    [#157048380] Added a function to mark notification as read on backend.

--- reactjs/reducers/notifications.js
+++ reactjs/reducers/notifications.js
@@ -4,11 +4,34 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'LESSON_NOTIFICATIONS_RECEIVED':
+    case 'NOTIFICATIONS_RECEIVED':
       return {
         ...state,
         notifications: action.notifications,
       };
+
+    case 'NOTIFICATIONS_MARK_AS_READ_IN_STORE': {
+      // Search for the notification.
+      const index = state.notifications.findIndex(element => element.id === action.notificationId);
+
+      // If the notification was found, then we should update it.
+      if (index !== -1) {
+        return {
+          ...state,
+          notifications: [
+            ...state.notifications.slice(0, index),
+            {
+              ...state.notifications[index],
+              isRead: true,
+            },
+            ...state.notifications.slice(index + 1),
+          ],
+        };
+      }
+
+      // Otherwise return unchanged state.
+      return state;
+    }
 
     default:
       return state;
--- reactjs/sagas/notifications.js
+++ reactjs/sagas/notifications.js
@@ -1,8 +1,9 @@
-import { all, put, takeLatest, apply, call } from 'redux-saga/effects';
+import { all, put, takeLatest, apply, call, select } from 'redux-saga/effects';
 import request from '../utils/request';
 import ClientAuth from '../auth/clientAuth';
 import * as api from '../api/notifications';
 import * as notificationsActions from '../actions/notifications';
+import * as arrayHelper from '../utils/array';
 
 /**
  * Fetch notifications from the backend.
@@ -31,10 +32,38 @@ function* fetchNotifications() {
 }
 
 /**
+ * Fetch notifications from the backend.
+ */
+function* markAsRead({ notificationId }) {
+  try {
+    const notifications = yield select(store => store.notifications.notifications);
+    const notificationItem = arrayHelper.getObjectById(notifications, notificationId);
+
+    // Making sure the request object includes the valid access token.
+    const auth = new ClientAuth();
+    const accessToken = yield apply(auth, auth.getAccessToken);
+    request.set('Authorization', `Bearer ${accessToken}`);
+
+    // Makes request to the backend to update comment.
+    yield call(
+      api.markAsRead,
+      request,
+      notificationItem.bundle,
+      notificationItem.uuid,
+    );
+  }
+  catch (error) {
+    console.error('Could not update list of notifications.', error);
+  }
+}
+
+/**
  * Main entry point for all notification sagas.
  */
 export default function* notificationsSagas() {
   yield all([
     yield takeLatest('NOTIFICATIONS_REQUESTED', fetchNotifications),
+    yield takeLatest('NOTIFICATIONS_MARK_AS_READ', markAsRead),
+    yield takeLatest('NOTIFICATIONS_MARK_ALL_AS_READ', fetchNotifications),
   ]);
 }
