f7523d154bdca0c7445b838073a9c51c8eabacf0
Andrei Tyuhai <atyuhai@krengeltech.com>
2018-05-14 14:23:35 +0300

    [#157085354] code refactoring

--- tests/codeception/tests/_support/Step/Acceptance/Learner.php
+++ tests/codeception/tests/_support/Step/Acceptance/Learner.php
@@ -6,20 +6,12 @@ class Learner extends \AcceptanceTester {
 
   public function loginAsLearner() {
     $I = $this;
-
-    // Use different password at platform.sh.
-    $password = isset($_ENV["TEST_USERS_PASS"])? $_ENV["TEST_USERS_PASS"]: 'password';
-
-    $I->login('authenticated.test', $password);
+    $I->login('authenticated.test', 'password', TRUE);
   }
 
   public function loginAsLearner2() {
     $I = $this;
-
-    // Use different password at platform.sh.
-    $password = isset($_ENV["TEST_USERS_PASS"])? $_ENV["TEST_USERS_PASS"]: 'password';
-
-    $I->login('authenticated2.test', $password);
+    $I->login('authenticated2.test', 'password', TRUE);
   }
 
 }
--- tests/codeception/tests/_support/Step/Acceptance/Manager.php
+++ tests/codeception/tests/_support/Step/Acceptance/Manager.php
@@ -6,11 +6,7 @@ class Manager extends \AcceptanceTester {
 
   public function loginAsManager() {
     $I = $this;
-
-    // Use different password at platform.sh.
-    $password = isset($_ENV["TEST_USERS_PASS"])? $_ENV["TEST_USERS_PASS"]: 'password';
-
-    $I->login('moderator.test', $password);
+    $I->login('moderator.test', 'password', TRUE);
   }
 
 }
