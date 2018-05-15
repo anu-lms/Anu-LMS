9c53e2824595317de4bf7f737a1509586a0a3255
ikorgik <ikorgik@gmail.com>
2018-05-15 10:26:57 +0300

    [#157049100] Added basic implementation of reply to comment classes.

--- drupal/web/modules/anu/anu_events/src/AnuEventInterface.php
+++ drupal/web/modules/anu/anu_events/src/AnuEventInterface.php
@@ -2,15 +2,19 @@
 
 namespace Drupal\anu_events;
 
-interface AnuEventInterface {
+use Drupal\Component\Plugin\DerivativeInspectionInterface;
+use Drupal\Component\Plugin\PluginInspectionInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+
+interface AnuEventInterface extends ContainerFactoryPluginInterface, PluginInspectionInterface, DerivativeInspectionInterface  {
 
   /**
    * Returns true if Event should be triggered.
    */
-  public function shouldTrigger($hook, $context);
+  public function shouldTrigger();
 
   /**
    * Check if event should be triggered, creates Message entity and notify channels.
    */
-  public function trigger($hook, $context);
+  public function trigger();
 }
