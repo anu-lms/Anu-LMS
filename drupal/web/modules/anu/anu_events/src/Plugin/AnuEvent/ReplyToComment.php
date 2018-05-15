eb3689042acd15f14ac471e268f313f9100821b6
ikorgik <ikorgik@gmail.com>
2018-05-15 11:22:08 +0300

    [#157049100] Added add comment to thread class.

--- drupal/web/modules/anu/anu_events/src/Plugin/AnuEvent/ReplyToComment.php
+++ drupal/web/modules/anu/anu_events/src/Plugin/AnuEvent/ReplyToComment.php
@@ -2,8 +2,7 @@
 
 namespace Drupal\anu_events\Plugin\AnuEvent;
 
-use Drupal\anu_events\AnuEventBase;
-use Psr\Log\LoggerInterface;
+use Drupal\anu_events\AnuEventCommentBase;
 
 /**
  * Reply to the Comment event.
@@ -14,36 +13,7 @@ use Psr\Log\LoggerInterface;
  *   description = @Translation("Triggered when user replied to the comment"),
  * )
  */
-class ReplyToComment extends AnuEventBase {
-
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerInterface $logger, $notifier_manager, $hook = '', array $context = []) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $logger, $notifier_manager, $hook, $context);
-
-    $this->entity = !empty($context['entity']) ? $context['entity'] : NULL;
-  }
-
-  function shouldTrigger() {
-    if ($this->hook !== 'entity_insert' || empty($this->entity)) {
-      return FALSE;
-    }
-
-    if ($this->entity->getEntityTypeId() !== 'paragraph_comment' || $this->entity->bundle() !== 'paragraph_comment') {
-      return FALSE;
-    }
-
-    // We shouldn't trigger event if Recipient and Triggerer the same.
-    // For example when user replies to his own comment.
-    return $this->getTriggerer() !== $this->getRecipient();
-  }
-
-  protected function getMessageBundle() {
-    // Use plugin Id as message bundle name by default.
-    return $this->pluginId;
-  }
-
-  protected function getTriggerer() {
-    return (int) $this->entity->uid->target_id;
-  }
+class ReplyToComment extends AnuEventCommentBase {
 
   protected function getRecipient() {
     if (!empty($this->entity->field_comment_parent->getValue())) {
@@ -55,10 +25,5 @@ class ReplyToComment extends AnuEventBase {
         ->target_id;
     }
     return NULL;
-  }
-
-  protected function attachMessageFields($message) {
-    $message->field_message_comment = $this->entity->id();
-    $message->field_message_recipient = $this->getRecipient();
   }
 }
