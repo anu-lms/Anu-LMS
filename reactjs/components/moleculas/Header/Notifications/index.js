73684ca53cbbef40f6a1aef2e48f68fcaa6099aa
ikorgik <ikorgik@gmail.com>
2018-05-16 20:42:14 +0300

    [#157394547] Fixed header icon styles on mobile.

--- reactjs/components/moleculas/Header/Notifications/index.js
+++ reactjs/components/moleculas/Header/Notifications/index.js
@@ -72,7 +72,7 @@ class Notifications extends React.Component {
       <div className={`notifications-wrapper ${isOpened ? 'popup-opened' : 'popup-closed'}`}>
 
         <div className="notifications-icons">
-          <HeaderIcon className="icon-bell" onClick={this.togglePopup} onKeyPress={this.togglePopup}>
+          <HeaderIcon className="icon-bell" label="Notifications" onClick={this.togglePopup} onKeyPress={this.togglePopup}>
             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="33" viewBox="0 0 28 33">
               <g fill="none" fillRule="evenodd">
                 <path fill="#FFF" fillRule="nonzero" d="M14 32.833c1.833 0 3.333-1.5 3.333-3.333h-6.666A3.332 3.332 0 0 0 14 32.833zm10-10V14.5c0-5.117-2.733-9.4-7.5-10.533V2.833c0-1.383-1.117-2.5-2.5-2.5a2.497 2.497 0 0 0-2.5 2.5v1.134C6.717 5.1 4 9.367 4 14.5v8.333L.667 26.167v1.666h26.666v-1.666L24 22.833z" />
