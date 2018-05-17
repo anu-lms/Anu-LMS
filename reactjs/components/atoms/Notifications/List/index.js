3ce54c70e846c9caabe42b6909f0c30b4ba0f5e5
ikorgik <ikorgik@gmail.com>
2018-05-17 11:05:01 +0300

    [#157630029] Wrapped popup to error boundaries.

---
+++ reactjs/components/atoms/Notifications/List/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Scrollbars } from 'react-custom-scrollbars';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import NotificationCommentItem, { supportedBundles as commentSupportedBundles } from '../CommentItem';
+
+const NotificationsList = ({
+  notifications, onCloseClick,
+  loadMore, hasMore, isLoading,
+}) => (
+  <div className="list">
+    <Scrollbars style={{ height: '100%' }}>
+      <InfiniteScroll
+        dataLength={notifications.length}
+        next={loadMore}
+        hasMore={hasMore}
+        height={460}
+      >
+        {notifications.map(item => {
+          if (commentSupportedBundles.indexOf(item.bundle) >= 0) {
+            return (
+              <NotificationCommentItem
+                notificationItem={item}
+                key={item.id}
+                closePopup={onCloseClick}
+              />
+            );
+          }
+          return null;
+        })}
+        <div className={`spinner ${isLoading ? 'show' : ''}`}>
+          <img src="/static/img/spinner-small.gif" alt="Loading..." />
+        </div>
+      </InfiniteScroll>
+    </Scrollbars>
+  </div>
+);
+
+NotificationsList.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  hasMore: PropTypes.bool.isRequired,
+  onCloseClick: PropTypes.func,
+  loadMore: PropTypes.func,
+  notifications: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+NotificationsList.defaultProps = {
+  onCloseClick: () => {},
+  loadMore: () => {},
+};
+
+export default NotificationsList;
