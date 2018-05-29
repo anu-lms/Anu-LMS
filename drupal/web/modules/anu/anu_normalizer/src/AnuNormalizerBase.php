2f023f2aedfaddd169d947afd1b2e34593638166
ikorgik <ikorgik@gmail.com>
2018-05-29 11:03:18 +0300

    [#157597114] Added comments.

--- drupal/web/modules/anu/anu_normalizer/src/AnuNormalizerBase.php
+++ drupal/web/modules/anu/anu_normalizer/src/AnuNormalizerBase.php
@@ -2,9 +2,9 @@
 
 namespace Drupal\anu_normalizer;
 
+use Psr\Log\LoggerInterface;
 use Drupal\Component\Plugin\PluginBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Psr\Log\LoggerInterface;
 
 abstract class AnuNormalizerBase extends PluginBase implements AnuNormalizerInterface {
 
@@ -15,14 +15,15 @@ abstract class AnuNormalizerBase extends PluginBase implements AnuNormalizerInte
 
   /**
    * Applies normalizer for given entity.
-   * Returns an object with fields given in $include_fields parameter.
+   * Returns normalized object with fields given in $include_fields parameter.
    */
   public abstract function normalize($entity, $include_fields);
 
   /**
-   *
+   * Normalize entity by appropriate AnuNormalizer plugin.
    */
   public static function normalizeEntity($entity, $include_fields = []) {
+    // Load list of existing plugins.
     $anu_normalizer_plugins = \Drupal::service('plugin.manager.anu_normalizer')->getDefinitions();
     foreach ($anu_normalizer_plugins as $anu_normalizer_plugin) {
       $anu_normalizer = \Drupal::service('plugin.manager.anu_normalizer')->createInstance($anu_normalizer_plugin['id']);
@@ -33,7 +34,8 @@ abstract class AnuNormalizerBase extends PluginBase implements AnuNormalizerInte
       }
     }
 
-    return $entity;
+    // Returns NULL if entity wasn't normalized.
+    return NULL;
   }
 
   /**
@@ -46,7 +48,7 @@ abstract class AnuNormalizerBase extends PluginBase implements AnuNormalizerInte
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
    * @param \Drupal\Core\Logger\LoggerChannelInterface $logger
-   *   The message_notify logger channel.
+   *   The anu_normalizer logger channel.
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerInterface $logger) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
