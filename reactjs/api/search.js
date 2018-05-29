269cd6609e9d8704cfa11f7fde5e0c35f23f7bc6
ikorgik <ikorgik@gmail.com>
2018-05-29 11:58:21 +0300

    [#157597114] Improved normalizer functions.

--- reactjs/api/search.js
+++ reactjs/api/search.js
@@ -1,4 +1,6 @@
 
+import * as dataProcessors from '../utils/dataProcessors';
+
 /**
  * Make a request to the backend to get user notifications.
  */
@@ -10,7 +12,26 @@ export const fetch = request => new Promise((resolve, reject) => {
       'filter[fulltext][condition][fulltext]': 'search', // @todo: replace hardcoded value.
     })
     .then(response => {
-      resolve(response.body);
+      // @todo: an example of normalization, improve where necessary.
+      const items = response.body.map(item => {
+        let normalizedEntity = null;
+        if (item.type === 'notebook') {
+          normalizedEntity = dataProcessors.notebookData(item.entity);
+        }
+        if (item.type === 'lesson') {
+          normalizedEntity = dataProcessors.lessonData(item.entity);
+        }
+        if (item.type === 'paragraph_comment') {
+          normalizedEntity = dataProcessors.processComment(item.entity);
+        }
+
+        return {
+          ...item,
+          entity: normalizedEntity,
+        };
+      });
+
+      resolve(items);
     })
     .catch(error => {
       console.log('Could not fetch search results.', error);
