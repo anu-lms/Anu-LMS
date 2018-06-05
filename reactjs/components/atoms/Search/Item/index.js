7231de4c21eac3e98f77f6c7f5b1e5a9d515fe7c
ikorgik <ikorgik@gmail.com>
2018-06-05 12:58:55 +0300

    [#157598052] Added lesson icon.

--- reactjs/components/atoms/Search/Item/index.js
+++ reactjs/components/atoms/Search/Item/index.js
@@ -4,12 +4,12 @@ import xss from 'xss';
 import classNames from 'classnames';
 import LinkWrap from '../../Link/LinkWrap';
 
-const SearchItem = ({ Icon, title, body, className, itemLink }) => (
+const SearchItem = ({ icon, title, body, className, itemLink }) => (
   <div className={classNames('search-item', className)}>
     <LinkWrap url={itemLink}>
-      {Icon &&
-      <div className="type-icon"><Icon /></div>
-      }
+      {icon &&
+      <div className="type-icon">{icon}</div>
+       }
       <div className="title">{title}</div>
       <div className="body" dangerouslySetInnerHTML={{ __html: xss(body) }} />
     </LinkWrap>
@@ -17,7 +17,7 @@ const SearchItem = ({ Icon, title, body, className, itemLink }) => (
 );
 
 SearchItem.propTypes = {
-  Icon: PropTypes.func,
+  icon: PropTypes.node,
   title: PropTypes.string.isRequired,
   body: PropTypes.string,
   className: PropTypes.string,
@@ -25,7 +25,7 @@ SearchItem.propTypes = {
 };
 
 SearchItem.defaultProps = {
-  Icon: null,
+  icon: null,
   body: '',
   className: '',
   itemLink: null,
--- reactjs/components/atoms/Search/LessonItem/index.js
+++ reactjs/components/atoms/Search/LessonItem/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SearchItem from '../Item';
-
-const Icon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-    <g fill="none" fillRule="evenodd">
-      <path fill="#B2B2B2" fillRule="nonzero" d="M19.99 2c0-1.1-.89-2-1.99-2H2C.9 0 0 .9 0 2v12c0 1.1.9 2 2 2h14l4 4-.01-18z" />
-      <path d="M-2-2h24v24H-2z" />
-    </g>
-  </svg>
-);
+import LessonIcon from '../../Icons/Lesson';
 
 class LessonSearchItem extends React.Component {
   render() {
@@ -18,7 +10,7 @@ class LessonSearchItem extends React.Component {
 
     return (
       <SearchItem
-        Icon={Icon}
+        icon={LessonIcon}
         title={title}
         body={excerpt}
         className={type}
