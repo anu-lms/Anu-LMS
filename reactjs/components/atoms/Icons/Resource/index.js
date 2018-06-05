afc6f4ee701e7f93906ed86445da3444c611f732
ikorgik <ikorgik@gmail.com>
2018-06-05 13:43:51 +0300

    [#157598052] Added Resource item in search results.

---
+++ reactjs/components/atoms/Icons/Resource/index.js
@@ -0,0 +1,8 @@
+export default (
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+    <g fill="none" fillRule="evenodd">
+      <path d="M-2-2h24v24H-2z" />
+      <path fill="#FFF" fillRule="nonzero" d="M18 0H6C4.9 0 4 .9 4 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM9.5 7.5C9.5 8.33 8.83 9 8 9H7v2H5.5V5H8c.83 0 1.5.67 1.5 1.5v1zm5 2c0 .83-.67 1.5-1.5 1.5h-2.5V5H13c.83 0 1.5.67 1.5 1.5v3zm4-3H17v1h1.5V9H17v2h-1.5V5h3v1.5zM7 7.5h1v-1H7v1zM2 4H0v14c0 1.1.9 2 2 2h14v-2H2V4zm10 5.5h1v-3h-1v3z" />
+    </g>
+  </svg>
+);
