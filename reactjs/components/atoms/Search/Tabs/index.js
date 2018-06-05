bbef4745d55cc4eda7b1c23343e4f480f542e3df
ikorgik <ikorgik@gmail.com>
2018-06-05 21:00:22 +0300

    [#157598052] Added styles for desktop.

--- reactjs/components/atoms/Search/Tabs/index.js
+++ reactjs/components/atoms/Search/Tabs/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const SearchTabs = () => (
   <div className="search-tabs">
-    <div className="inner">
-      <span className="active">All</span>
-      <span>Media</span>
-      <span>Resources</span>
+    <div className="inner-wrapper">
+      <div className="inner">
+        <span className="active">All</span>
+        <span>Media</span>
+        <span>Resources</span>
+      </div>
     </div>
   </div>
 );
