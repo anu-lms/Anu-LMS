9dff0a2e92995da58c93494491a1bf802c876bab
spleshka <spleshka@gmail.com>
2018-06-06 21:16:46 +0300

    [#157597331] Avoid animation on empty & one-char search queries.

--- reactjs/sagas/search.js
+++ reactjs/sagas/search.js
@@ -16,21 +16,23 @@ const backendSearchDelay = 350;
  */
 function* fetchSearch({ text }) {
   try {
-    // There's a small delay between a user finishing the typing and frontend
-    // sending request to search. It is introduced in order to avoid flooding
-    // the backend with tons of requests if a user types in fast. If a user
-    // is typing fast, then saga will automatically cancel this function and
-    // it will not get to the backend request. If a user is typing slowly,
-    // then it is expected to refresh search results as he types.
-    yield delay(backendSearchDelay);
-
-    // Making sure the request object includes the valid access token.
-    const auth = new ClientAuth();
-    const accessToken = yield apply(auth, auth.getAccessToken);
-    request.set('Authorization', `Bearer ${accessToken}`);
 
     let searchResults = [];
     if (text && text.length > 1) {
+
+      // There's a small delay between a user finishing the typing and frontend
+      // sending request to search. It is introduced in order to avoid flooding
+      // the backend with tons of requests if a user types in fast. If a user
+      // is typing fast, then saga will automatically cancel this function and
+      // it will not get to the backend request. If a user is typing slowly,
+      // then it is expected to refresh search results as he types.
+      yield delay(backendSearchDelay);
+
+      // Making sure the request object includes the valid access token.
+      const auth = new ClientAuth();
+      const accessToken = yield apply(auth, auth.getAccessToken);
+      request.set('Authorization', `Bearer ${accessToken}`);
+
       // Makes request to the backend to fetch search results.
       searchResults = yield call(api.fetch, request, text);
     }
