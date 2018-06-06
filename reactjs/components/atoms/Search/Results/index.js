c051a97775872627f6edc8b748602a196ac3fa80
ikorgik <ikorgik@gmail.com>
2018-06-06 14:48:18 +0300

    [#157598052] Show tab when user scroll down.

--- reactjs/components/atoms/Search/Results/index.js
+++ reactjs/components/atoms/Search/Results/index.js
@@ -13,48 +13,83 @@ const availableSearchComponents = {
   'media_resource': dynamic(import('../ResourceItem')),
 };
 
-const SearchResults = ({ query, results, isFetched, isError }) => (
-  <div className="search-container">
-    {query && query.length > 1 &&
-    <SearchTabs />
+class SearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showTabsBorder: false,
+    };
+
+    this.toggleTabsBorder = this.toggleTabsBorder.bind(this);
+  }
+
+  /**
+   * Show border under tabs when user scroll down.
+   */
+  toggleTabsBorder() {
+    const tabs = document.getElementById('search-tabs');
+    const list = document.getElementById('search-results-list');
+    const tabsBottom = tabs.getBoundingClientRect().bottom;
+
+    if (!this.state.showTabsBorder && list.getBoundingClientRect().top < tabsBottom) {
+      this.setState({ showTabsBorder: true });
     }
+    else if (this.state.showTabsBorder && list.getBoundingClientRect().top >= tabsBottom) {
+      this.setState({ showTabsBorder: false });
+    }
+  }
 
-    <div className="search-results">
-      <Scrollbars style={{ height: '100%' }}>
-        <div className="inner-wrapper">
-          <div className="inner">
+  render() {
+    const { query, results, isFetched, isError } = this.props;
 
-            {!isError && results.length > 0 &&
-            <div className="list">
-              {results.map(resultItem => {
-                const SearchItemComponent = availableSearchComponents[resultItem.type];
-                if (SearchItemComponent) {
-                  return <SearchItemComponent key={resultItem.entity.uuid} searchItem={resultItem} />;
+    return (
+      <div className="search-container">
+        {query && query.length > 1 &&
+        <SearchTabs showBorder={this.state.showTabsBorder} />
+        }
+
+        <div className="search-results">
+          <Scrollbars style={{ height: '100%' }} onScroll={this.toggleTabsBorder}>
+            <div id="search-results-list" className="inner-wrapper">
+              <div className="inner">
+
+                {!isError && results.length > 0 &&
+                <div className="list">
+                  {results.map(resultItem => {
+                    const SearchItemComponent = availableSearchComponents[resultItem.type];
+                    if (SearchItemComponent) {
+                      // eslint-disable-next-line max-len
+                      return <SearchItemComponent key={resultItem.entity.uuid} searchItem={resultItem} />;
+                    }
+
+                    return null;
+                  })}
+                </div>
                 }
 
-                return null;
-              })}
-            </div>
-            }
+                {query && query.length > 1 && results.length === 0 && isFetched && !isError &&
+                <Empty />
+                }
 
-            {query && query.length > 1 && results.length === 0 && isFetched && !isError &&
-              <Empty />
-            }
-
-            {isError &&
-              <div className="search-error">
-              The error has occurred. Please try to reload the page or contact site administrator.
+                {isError &&
+                <div className="search-error">
+                  {/* eslint-disable-next-line max-len */}
+                  The error has occurred. Please try to reload the page or contact site administrator.
+                </div>
+                }
               </div>
-            }
-          </div>
+            </div>
+          </Scrollbars>
         </div>
-      </Scrollbars>
-    </div>
-  </div>
-);
+      </div>
+    );
+  }
+}
 
 SearchResults.propTypes = {
   results: PropTypes.arrayOf(PropTypes.shape),
+  query: PropTypes.string,
   isFetching: PropTypes.bool,
   isFetched: PropTypes.bool,
   isError: PropTypes.bool,
@@ -62,6 +97,7 @@ SearchResults.propTypes = {
 
 SearchResults.defaultProps = {
   results: [],
+  query: '',
   isFetching: false,
   isFetched: false,
   isError: false,
--- reactjs/components/atoms/Search/Tabs/index.js
+++ reactjs/components/atoms/Search/Tabs/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-const SearchTabs = () => (
-  <div className="search-tabs">
+const SearchTabs = ({ showBorder }) => (
+  <div id="search-tabs" className={classNames('search-tabs', { 'show-border': showBorder })}>
     <div className="inner-wrapper">
       <div className="inner">
         <span className="active">All</span>
@@ -11,5 +13,13 @@ const SearchTabs = () => (
     </div>
   </div>
 );
+
+SearchTabs.propTypes = {
+  showBorder: PropTypes.bool,
+};
+
+SearchTabs.defaultProps = {
+  showBorder: false,
+};
 
 export default SearchTabs;
