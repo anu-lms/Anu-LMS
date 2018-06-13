5fa5bf6b03d660c6fcbec936935ed70f312c4de3
ikorgik <ikorgik@gmail.com>
2018-06-13 12:28:45 +0300

    [#157631392] Hid search input in header when overlay opened.

--- reactjs/components/moleculas/Header/Search/index.js
+++ reactjs/components/moleculas/Header/Search/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import SearchIcon from '../../../atoms/Search/Icon';
@@ -10,6 +10,9 @@ import * as overlayActions from '../../../../actions/overlay';
 class Search extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isOverlayOpened: false,
+    };
 
     this.onHeaderSearchClick = this.onHeaderSearchClick.bind(this);
     this.onOverlayClose = this.onOverlayClose.bind(this);
@@ -24,24 +27,33 @@ class Search extends React.Component {
 
     // Open an overlay with search components inside of it.
     dispatch(overlayActions.open(content, header, this.onOverlayClose));
+
+    // Hide search input in header when overlay opened.
+    this.setState({ isOverlayOpened: true });
   }
 
   onOverlayClose() {
     this.props.dispatch(searchActions.clear());
+
+    // Show back search input in header when overlay closed.
+    this.setState({ isOverlayOpened: false });
   }
 
   render() {
     return (
-      <div className="search" onClick={this.onHeaderSearchClick} onKeyPress={() => {}}>
+      <Fragment>
+        {!this.state.isOverlayOpened &&
+        <div className="search" onClick={this.onHeaderSearchClick} onKeyPress={() => {}}>
+          <input type="text" placeholder="Search" value="" onChange={() => {}} />
 
-        <input type="text" placeholder="Search" value="" onChange={() => {}} />
+          <div className="header-icon search-bar">
+            <div className="icon"><SearchIcon /></div>
+            <div className="label">Search</div>
+          </div>
 
-        <div className="header-icon search-bar">
-          <div className="icon"><SearchIcon /></div>
-          <div className="label">Search</div>
         </div>
-
-      </div>
+      }
+      </Fragment>
     );
   }
 }
