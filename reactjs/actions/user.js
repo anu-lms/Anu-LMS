3ab048e723218bcadc435a35656620c53e7e8fff
ikorgik <ikorgik@gmail.com>
2018-06-15 15:34:11 +0300

    [#157910554] Removed session token from actions.

--- reactjs/actions/user.js
+++ reactjs/actions/user.js
@@ -1,14 +1,6 @@
-export function login(uid, sessionToken) {
+export function login(uid) {
   return {
     type: 'USER_LOGIN',
     uid,
-    sessionToken,
-  };
-}
-
-export function updateSessionToken(sessionToken) {
-  return {
-    type: 'USER_UPDATE_SESSION_TOKEN',
-    sessionToken,
   };
 }
