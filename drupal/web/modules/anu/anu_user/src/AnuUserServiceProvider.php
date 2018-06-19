5a42abfa6e238b3935dd9c40acc08ca098892dd8
ikorgik <ikorgik@gmail.com>
2018-06-19 18:17:29 +0300

    [#158422171] Fixed an issue with csrf token.

--- drupal/web/modules/anu/anu_user/src/AnuUserServiceProvider.php
+++ drupal/web/modules/anu/anu_user/src/AnuUserServiceProvider.php
@@ -14,5 +14,9 @@ class AnuUserServiceProvider extends ServiceProviderBase {
     // Replaces default core's auth class with our custom class.
     $definition = $container->getDefinition('user.auth');
     $definition->setClass('Drupal\anu_user\UserAuth');
+
+    // Replaces default core's CSRF validator class with our custom class (see comments inside class for more info).
+    $definition = $container->getDefinition('access_check.header.csrf');
+    $definition->setClass('Drupal\anu_user\CsrfRequestHeaderAccessCheck');
   }
 }
