83c6e2748f155e3366dc28bcfc52676535d86a7b
ikorgik <ikorgik@gmail.com>
2018-06-23 02:38:35 +0300

    [#158570251] Reverted xss filter in paragraph text (workaround).

--- reactjs/components/atoms/Paragraph/Text/Text.js
+++ reactjs/components/atoms/Paragraph/Text/Text.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import xss from 'xss';
 import ShowCommentsCTA from '../../../moleculas/Lesson/ShowCommentsCTA';
 
 class Text extends React.Component {
