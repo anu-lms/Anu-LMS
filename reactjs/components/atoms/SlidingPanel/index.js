11289683371fc8b87d71d7b9ec5a4810006e3340
ikorgik <ikorgik@gmail.com>
2018-06-27 12:09:55 +0300

    [#157774616] Added implementation for sliding panel.

--- reactjs/components/atoms/SlidingPanel/index.js
+++ reactjs/components/atoms/SlidingPanel/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 
 class SlidingPanel extends React.Component {
   render() {
-    const { isOpened, className, children } = this.props;
+    const { isOpened, onClose, className, children } = this.props;
 
     return (
-      <div className={classNames('sliding-panel-container', className, { 'opened': isOpened })} >
-        {children}
-      </div>
+      <Fragment>
+        <div className={classNames('sliding-panel-container', className, { 'opened': isOpened })} >
+          {children}
+        </div>
+
+        {isOpened &&
+        <div className="scrim" onClick={onClose} onKeyPress={onClose} />
+        }
+      </Fragment>
     );
   }
 }
