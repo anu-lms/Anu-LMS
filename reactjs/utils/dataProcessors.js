aa79299adac01b18cd1de5064e4d894e7e675939
spleshka <spleshka@gmail.com>
2018-06-27 17:49:48 +0300

    [#158592363] Fixed eslint warning.

--- reactjs/utils/dataProcessors.js
+++ reactjs/utils/dataProcessors.js
@@ -41,17 +41,15 @@ export const courseData = course => {
 /**
  * Internal helper to process lesson data from the backend.
  */
-export const lessonData = lesson => {
-  return {
-    id: lesson.nid,
-    uuid: lesson.uuid,
-    // eslint-lesson-next-line max-len
-    url: lessonHelper.getUrl(lesson.fieldLessonCourse.path.alias, lesson.path.alias),
-    title: lesson.title,
-    isAssessment: lesson.fieldIsAssessment ? lesson.fieldIsAssessment : false,
-    progress: 0,
-  };
-};
+export const lessonData = lesson => ({
+  id: lesson.nid,
+  uuid: lesson.uuid,
+  // eslint-lesson-next-line max-len
+  url: lessonHelper.getUrl(lesson.fieldLessonCourse.path.alias, lesson.path.alias),
+  title: lesson.title,
+  isAssessment: lesson.fieldIsAssessment ? lesson.fieldIsAssessment : false,
+  progress: 0,
+});
 
 /**
  * Internal helper to normalize notebook note data from the backend.
