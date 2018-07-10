0c741df337c796eb9083caf5ef7d560bc0d5244a
ikorgik <ikorgik@gmail.com>
2018-07-10 13:52:25 +0300

    [#158629289] Added computed is Read property.

--- drupal/web/modules/anu/anu_comments/src/CommentReadStatus.php
+++ drupal/web/modules/anu/anu_comments/src/CommentReadStatus.php
@@ -2,11 +2,19 @@
 namespace Drupal\anu_comments;
 
 use Drupal\Core\TypedData\TypedData;
+use Drupal\Core\Language\LanguageInterface;
 
 /**
  * A property that returns TRUE if current user has read the comment.
  */
 class CommentReadStatus extends TypedData {
+
+  /**
+   * The langcode of the field values held in the object.
+   *
+   * @var string
+   */
+  protected $langcode = LanguageInterface::LANGCODE_NOT_SPECIFIED;
 
   /**
    * Is read state.
@@ -16,24 +24,33 @@ class CommentReadStatus extends TypedData {
   protected $value;
 
   /**
-   * @var \Drupal\Core\Field\FieldItemInterface
-   */
-  protected $parent;
-
-  /**
    * {@inheritdoc}
    */
   public function getValue() {
     if ($this->value === NULL) {
       $existing_entities_amount = \Drupal::entityQuery('paragraph_comment_read')
         ->condition('uid', \Drupal::currentUser()->id())
-        ->condition('field_comment', $this->parent->getEntity()->id())
+        ->condition('field_comment', $this->getParent()->getValue()->id())
         ->count()
         ->execute();
 
       $this->value = $existing_entities_amount > 0;
     }
     return $this->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setLangcode($langcode) {
+    $this->langcode = $langcode;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLangcode() {
+    return $this->langcode;
   }
 
 }
