675963f1689f50917779567b4808552e1d6105df
ikorgik <ikorgik@gmail.com>
2018-07-19 16:15:52 +0300

    [#158629151] Added basic implementation for sticky bar.

--- reactjs/components/atoms/Comment/NewCommentsBar/index.js
+++ reactjs/components/atoms/Comment/NewCommentsBar/index.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import Sticky from '../../Sticky';
 
 const NewCommentsBar = ({ newCommentsAmount }) => (
-  <div className="new-comments-bar">
-    <span className="arrow">
-      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12">
-        <g fill="none" fillRule="evenodd">
-          <path fill="#FFF" fillRule="nonzero" d="M11.333 6l-.94-.94-3.726 3.72V.667H5.333V8.78l-3.72-3.727L.667 6 6 11.333z" />
-        </g>
-      </svg>
-    </span>
+  <Sticky className="new-comments-sticky" enter={50} rootId="lesson-comments-scrollable">
+    <div className="new-comments-bar">
+      <span className="arrow">
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12">
+          <g fill="none" fillRule="evenodd">
+            <path fill="#FFF" fillRule="nonzero" d="M11.333 6l-.94-.94-3.726 3.72V.667H5.333V8.78l-3.72-3.727L.667 6 6 11.333z" />
+          </g>
+        </svg>
+      </span>
 
-    {newCommentsAmount} new comments
+      {newCommentsAmount} new comments
 
-    <button className="close-button">
-      <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10">
-        <g fill="none" fillRule="evenodd">
-          <path fill="#FFF" fillRule="nonzero" d="M9.667 1.273l-.94-.94L5 4.06 1.273.333l-.94.94L4.06 5 .333 8.727l.94.94L5 5.94l3.727 3.727.94-.94L5.94 5z" />
-        </g>
-      </svg>
-    </button>
-  </div>
+      <button className="close-button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10">
+          <g fill="none" fillRule="evenodd">
+            <path fill="#FFF" fillRule="nonzero" d="M9.667 1.273l-.94-.94L5 4.06 1.273.333l-.94.94L4.06 5 .333 8.727l.94.94L5 5.94l3.727 3.727.94-.94L5.94 5z" />
+          </g>
+        </svg>
+      </button>
+    </div>
+  </Sticky>
 );
 
 export default NewCommentsBar;
