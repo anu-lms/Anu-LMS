9213e834df79fbcd4289382c02c984f2f4686707
ikorgik <ikorgik@gmail.com>
2018-08-03 10:18:41 +0300

    [#158630168] Added comments to the code.

--- reactjs/components/atoms/Comment/CommentBodyWithMentions/index.js
+++ reactjs/components/atoms/Comment/CommentBodyWithMentions/index.js
@@ -1,24 +1,26 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import processString from 'react-process-string';
 import _find from 'lodash/find';
 import Tooltip from 'rc-tooltip';
+import processString from 'react-process-string';
 import Avatar from '../../User/Avatar';
 import { getUsername } from '../../../../helpers/user';
 
 class CommentBodyWithMentions extends React.Component {
   render() {
     const { text, mentions } = this.props;
-    const match = /<span class=\'tagged-user\'>@([^<>]*)<\/span>/g;
+    const match = /<span class='tagged-user'>@([^<>]*)<\/span>/g;
     let config = [{
       regex: match,
       fn: (key, result) => {
+        // Find mentioned user in the array.
         const mentionedUser = _find(mentions, item => item.name === result[1]);
         if (!mentionedUser) {
           return <span key={key} className="tagged-user">@{result[1]}</span>;
         }
 
-        const overlay = <Fragment><Avatar user={mentionedUser} /> <span>{getUsername(mentionedUser)}</span></Fragment>;
+        // Show tooltip with data about user.
+        const overlay = <Fragment><Avatar user={mentionedUser} /> <span>{getUsername(mentionedUser)}</span></Fragment>; // eslint-disable-line max-len
         return (
           <Tooltip
             key={key}
@@ -45,6 +47,14 @@ class CommentBodyWithMentions extends React.Component {
   }
 }
 
-CommentBodyWithMentions.propTypes = {};
+CommentBodyWithMentions.propTypes = {
+  text: PropTypes.string,
+  mentions: PropTypes.arrayOf(PropTypes.object),
+};
+
+CommentBodyWithMentions.defaultProps = {
+  text: '',
+  mentions: [],
+};
 
 export default CommentBodyWithMentions;
