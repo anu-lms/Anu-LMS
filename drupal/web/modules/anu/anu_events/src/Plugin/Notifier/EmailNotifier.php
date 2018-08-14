975d7f0c92d5a28f83f2f4d93703a82867a5fd93
ikorgik <ikorgik@gmail.com>
2018-08-14 17:13:25 +0300

    [#159010760] Added should trigger method.

--- drupal/web/modules/anu/anu_events/src/Plugin/Notifier/EmailNotifier.php
+++ drupal/web/modules/anu/anu_events/src/Plugin/Notifier/EmailNotifier.php
@@ -14,6 +14,34 @@ namespace Drupal\anu_events\Plugin\Notifier;
 class EmailNotifier extends MessageNotifierBase {
 
   /**
+   * Returns TRUE if channel should be triggered.
+   */
+  public function shouldTrigger() {
+    try {
+      $bundle = $this->message->bundle();
+      // Gets recipient object from the message entity.
+      $recipient = $this->message->field_message_recipient->first()->get('entity')->getValue();
+      if (in_array($bundle, ['add_comment_to_thread', 'reply_to_comment'])) {
+
+        // Returns FALSE if user didn't set Notify if replied option.
+        return (bool) $recipient->field_notify_if_replied->getString();
+      }
+      elseif ($bundle == 'mentioned_in_comment') {
+
+        // Returns FALSE if user didn't set Notify if tagged option.
+        return (bool) $recipient->field_notify_if_tagged->getString();
+      }
+    }
+    catch (\Exception $exception) {
+      \Drupal::logger('anu_events')
+        ->critical('Could not execute shouldTrigger function. Error: @error', [
+          '@error' => $exception->getMessage(),
+        ]);
+    }
+    return FALSE;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function deliver(array $output = []) {
