a64715a1b6d72b5e48414493b78eb691e0e75c13
ikorgik <ikorgik@gmail.com>
2018-08-28 17:45:26 +0300

    [#155899383] Added an access check for groups.

---
+++ drupal/web/modules/anu/anu_group/src/Group.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\anu_group;
+
+use Drupal\group\Entity\GroupContent;
+
+/**
+ * Helper class for working with groups.
+ *
+ * @package Drupal\anu_group
+ */
+class Group {
+
+  /**
+   * Loads ids of group by given entity.
+   */
+  function getGroupIdsByEntity($entity) {
+    $group_ids = [];
+    $group_contents = GroupContent::loadByEntity($entity);
+    foreach ($group_contents as $group_content) {
+      $group_ids[] = $group_content->getGroup()->id();
+    }
+
+    return $group_ids;
+  }
+}
