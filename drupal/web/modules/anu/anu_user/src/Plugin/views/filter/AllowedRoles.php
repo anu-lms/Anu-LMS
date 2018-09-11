586b9d3296a42e96c4d5d8e747017d4beb08c43c
ikorgik <ikorgik@gmail.com>
2018-09-11 10:53:03 +0300

    [#159953558] Improved AllowedRoles views filter.

--- drupal/web/modules/anu/anu_user/src/Plugin/views/filter/AllowedRoles.php
+++ drupal/web/modules/anu/anu_user/src/Plugin/views/filter/AllowedRoles.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\anu_user\Plugin\views\filter;
 
-use Drupal\user\Plugin\views\filter\Roles;
+use Drupal\views\ViewExecutable;
 use Drupal\Core\Database\Query\Condition;
+use Drupal\views\Plugin\views\filter\InOperator;
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
 
 /**
  * Filter handler for user roles.
@@ -12,25 +14,44 @@ use Drupal\Core\Database\Query\Condition;
  *
  * @ViewsFilter("user_allowed_roles")
  */
-class AllowedRoles extends Roles {
+class AllowedRoles extends InOperator {
 
   /**
    * {@inheritdoc}
-   *
-   * Replace the configured permission with a filter by all roles that have this
-   * permission.
+   */
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+    $this->definition['options callback'] = [$this, 'generateOptions'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function canExpose() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function operators() {
+    $operators = [
+      'in' => [
+        'title' => $this->t('Is one of'),
+        'short' => $this->t('in'),
+        'short_single' => $this->t('='),
+        'method' => 'opSimple',
+        'values' => 1,
+      ],
+    ];
+
+    return $operators;
+  }
+
+  /**
+   * {@inheritdoc}
    */
   public function query() {
-//    $allowed_roles = \Drupal::service('delegatable_roles')->getAssignableRoles(\Drupal::currentUser());
-//    $this->value = [];
-//    foreach (array_keys($allowed_roles) as $rid) {
-//      $this->value[$rid] = $rid;
-//    }
-//
-//    parent::query();
-
-
-
     $allowed_roles = \Drupal::service('delegatable_roles')->getAssignableRoles(\Drupal::currentUser());
 
     $join = $this->getJoin();
@@ -45,6 +66,15 @@ class AllowedRoles extends Roles {
     $or->isNull($field);
 
     $this->query->addWhere($this->options['group'], $or);
+  }
+
+  /**
+   * Generate list of options for filter.
+   */
+  public function generateOptions() {
+    return [
+      'allowed_roles' => $this->t('Allowed roles for current user')
+    ];
   }
 
 }
