0170a24802928bedb716519b93fe4163a2bcaec8
ikorgik <ikorgik@gmail.com>
2018-10-10 23:12:36 +0300

    [#159233293] Improved assign organization action.

--- drupal/web/modules/anu/anu_user/src/Plugin/Action/AssignOrganization.php
+++ drupal/web/modules/anu/anu_user/src/Plugin/Action/AssignOrganization.php
@@ -54,24 +54,10 @@ class AssignOrganization extends ViewsBulkOperationsActionBase {
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-
-    $organizations = \Drupal::entityTypeManager()
-      ->getStorage('taxonomy_term')
-      ->loadByProperties([
-        'vid' => 'organisations',
-      ]);
-
-    $organization_list = [];
-    foreach ($organizations as $organization) {
-      if ($organization->access('view')) {
-        $organization_list[$organization->id()] = $organization->label();
-      }
-    }
-
     $form['organization'] = [
       '#title' => t('Choose the organization'),
       '#type' => 'checkboxes',
-      '#options' => $organization_list,
+      '#options' => \Drupal::service('anu_user.user')->getAllowedOrganizationsList(),
     ];
 
     return $form;
