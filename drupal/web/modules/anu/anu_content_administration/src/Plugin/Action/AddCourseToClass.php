f3b8a1e11860ce6a551b0c170f45e6282f82ae07
ikorgik <ikorgik@gmail.com>
2018-10-11 11:47:18 +0300

    [#159233293] Added courses to the same orgs as classes.

--- drupal/web/modules/anu/anu_content_administration/src/Plugin/Action/AddCourseToClass.php
+++ drupal/web/modules/anu/anu_content_administration/src/Plugin/Action/AddCourseToClass.php
@@ -31,11 +31,36 @@ class AddCourseToClass extends ViewsBulkOperationsActionBase {
       ->getStorage('group')
       ->loadMultiple($this->configuration['classes']);
 
+    $group_org_ids = [];
     /** @var \Drupal\group\Entity\GroupInterface $group */
     foreach ($groups as $group) {
+      // Add course to the choosen class.
       if (!$group->getContentByEntityId('group_node:course', $entity->id())) {
         $group->addContent($entity, 'group_node:course');
       }
+
+      // Collect organizations of all groups to add course to the same orgs.
+      if (!empty($group->field_organization->getValue())) {
+        $ids = array_column($group->field_organization->getValue(), 'target_id');
+        // Make sure we don't add same organizations twise.
+        $group_intersect = array_intersect($ids, $group_org_ids);
+        if (empty($group_intersect)) {
+          $group_org_ids = array_merge($group_org_ids, array_column($group->field_organization->getValue(), 'target_id'));
+        }
+      }
+    }
+
+    $course_org_ids = [];
+    // Get organization ids from given course.
+    if (!empty($entity->field_course_organisation->getValue())) {
+      $course_org_ids = array_column($entity->field_course_organisation->getValue(), 'target_id');
+    }
+
+    // Add course to the same organizations as Class.
+    $intersect = array_intersect($course_org_ids, $group_org_ids);
+    if (empty($intersect)) {
+      $entity->field_course_organisation = array_merge($course_org_ids, $group_org_ids);
+      $entity->save();
     }
   }
 
