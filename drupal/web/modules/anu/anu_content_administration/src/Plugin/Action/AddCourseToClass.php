aa633e6c0fc6a450c9cf5f376d8ab1171694a051
ikorgik <ikorgik@gmail.com>
2018-10-31 12:16:18 +0300

    [#161475427] Removed unnecessary functionality.

--- drupal/web/modules/anu/anu_content_administration/src/Plugin/Action/AddCourseToClass.php
+++ drupal/web/modules/anu/anu_content_administration/src/Plugin/Action/AddCourseToClass.php
@@ -31,39 +31,12 @@ class AddCourseToClass extends ViewsBulkOperationsActionBase {
       ->getStorage('group')
       ->loadMultiple($this->configuration['classes']);
 
-    $group_org_ids = [];
     /** @var \Drupal\group\Entity\GroupInterface $group */
     foreach ($groups as $group) {
       // Add course to the chosen class.
       if (!$group->getContentByEntityId('group_node:course', $entity->id())) {
         $group->addContent($entity, 'group_node:course');
       }
-
-      // Collect organizations of all groups to add course to the same orgs.
-      if (!empty($group->field_organization->getValue())) {
-        $group_org_ids = array_merge($group_org_ids, array_column($group->field_organization->getValue(), 'target_id'));
-      }
-    }
-
-    $course_org_ids = [];
-    // Get organization ids from given course.
-    if (!empty($entity->field_course_organisation->getValue())) {
-      $course_org_ids = array_column($entity->field_course_organisation->getValue(), 'target_id');
-    }
-
-    // Add course to the same organizations as Class.
-    $need_update = FALSE;
-    foreach ($group_org_ids as $group_org_id) {
-      if (!in_array($group_org_id, $course_org_ids)) {
-        $course_org_ids[] = $group_org_id;
-        $need_update = TRUE;
-      }
-    }
-
-    // Update course organizations if needed.
-    if ($need_update) {
-      $entity->field_course_organisation = $course_org_ids;
-      $entity->save();
     }
   }
 
