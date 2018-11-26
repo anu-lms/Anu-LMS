78d99663e0689965e6ade595f26096327cc7fd73
ikorgik <ikorgik@gmail.com>
2018-11-26 15:43:55 +0300

    [#162050434] Outputted onboarding link.

---
+++ drupal/web/modules/anu/anu_organization/src/Plugin/views/field/OnboardingLink.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\anu_organization\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\Component\Render\FormattableMarkup;
+use Drupal\views\ResultRow;
+
+/**
+ * Outputs onboarding link.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("anu_onboarding_link")
+ */
+class OnboardingLink extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $output = '';
+    $organization = $values->_entity;
+
+    try {
+      $output = 'onboarding link ' . $organization->id();
+    }
+    catch (\Exception $exception) {
+      $message = new FormattableMarkup('Could not generate onboarding link for organization @id. Error: @error', [
+        '@id' => $organization->id(),
+        '@error' => $exception->getMessage(),
+      ]);
+      \Drupal::logger('anu_organization')->error($message);
+    }
+
+    return $output;
+  }
+
+}
