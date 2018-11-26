94508c266216b01b4c72882b3349c25b2467d4a3
ikorgik <ikorgik@gmail.com>
2018-11-26 19:17:52 +0300

    [#162050434] Added basic implementation of getOnboarding link method.

--- drupal/web/modules/anu/anu_organization/src/Organization.php
+++ drupal/web/modules/anu/anu_organization/src/Organization.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\anu_organization;
 
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
 /**
  * Helper class for working with organizations.
  *
@@ -24,8 +27,25 @@ class Organization {
    * Generates onboarding link.
    */
   public function getOnboardingLink($organization) {
+    // hash with stored in db keyword.
+    // organization=6+9997&classes=12+34+54 => hash to => asd324fsasd235cssa23fasd3
+    $link = Url::fromUri(
+      'http://app.docker.localhost/user/register',
+      ['query' => ['token' => $organization->uuid()]]
+    );
+    return Link::fromTextAndUrl(
+      $link->toString(),
+      $link
+    )->toString();
+  }
 
-    return $organization->id();
+  /**
+   * Returns organization id fetched from onboarding link.
+   */
+  public function getOrganizationFromOnboardingLink($link) {
+    $term = \Drupal::service('entity.repository')->loadEntityByUuid('taxonomy_term', '0d5ec336-2b88-446f-bdc7-0ea9a64f6152');
+    $organizationId = 9997;
+    return $organizationId;
   }
 
 }
