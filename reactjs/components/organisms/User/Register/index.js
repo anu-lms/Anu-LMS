063db9c2c054292168f615416697fbe176bb5c13
ikorgik <ikorgik@gmail.com>
2018-11-28 15:41:57 +0300

    [#162050488] Implemented registration page template.

--- reactjs/components/organisms/User/Register/index.js
+++ reactjs/components/organisms/User/Register/index.js
@@ -2,38 +2,26 @@ import React, { Fragment } from 'react';
 import UserRegisterForm from '../../../moleculas/Form/UserRegister';
 import OneColumnLayout from '../../../../components/organisms/Templates/OneColumnLayout';
 
-/* eslint-disable max-len */
 class UserRegister extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      isEmailSent: false,
-    };
-
-    this.recoveryEmailSent = this.recoveryEmailSent.bind(this);
-  }
-
-  recoveryEmailSent() {
-    this.setState({ isEmailSent: true });
   }
 
   render() {
-    const { isEmailSent } = this.state;
-    const pageTitle = isEmailSent ? 'Recovery Email Sent' : 'Registration';
-    const layoutClasses = isEmailSent ? 'page-title-green' : 'short';
+    const tokenValidation = this.props.tokenValidation;
+    console.log(this.props);
     return (
       <div>
-        <OneColumnLayout pageTitle={pageTitle} className={layoutClasses}>
-          {!isEmailSent &&
-            <Fragment>
-              <div className="form-description">Enter your username or email address below, and we’ll send you an email with a link to reset your password.</div>
-              <UserRegisterForm />
-            </Fragment>
-          }
-
-          {isEmailSent &&
-            <div>Thank you! An email has been sent to your email address with a link to reset your password.</div>
+        <OneColumnLayout pageTitle="Registration" className="short">
+          {tokenValidation.isValid ? (
+              <Fragment>
+                <div className="form-description">Enter your username or email address below, and we’ll send you an email with a link to reset your password.</div>
+                <UserRegisterForm />
+              </Fragment>
+            ) : (
+              <div>{tokenValidation.errorMessage}</div>
+            )
           }
         </OneColumnLayout>
       </div>
