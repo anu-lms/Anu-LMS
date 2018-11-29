3c53c7df5fe4e61d4eac93cc70b5ba817350198e
ikorgik <ikorgik@gmail.com>
2018-11-29 13:50:10 +0300

    [#162050488] Added a code to register user.

--- drupal/web/modules/anu/anu_user/src/Plugin/rest/resource/UserRegistrationResource.php
+++ drupal/web/modules/anu/anu_user/src/Plugin/rest/resource/UserRegistrationResource.php
@@ -5,11 +5,10 @@ namespace Drupal\anu_user\Plugin\rest\resource;
 use Psr\Log\LoggerInterface;
 use Drupal\user\UserStorageInterface;
 use Drupal\rest\ResourceResponse;
-use Drupal\Component\Utility\Crypt;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\anu_normalizer\AnuNormalizerBase;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\rest\Plugin\rest\resource\EntityResourceValidationTrait;
 
 /**
  * Provides a resource to validate reset password link and reset password.
@@ -18,11 +17,12 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   id = "user_registration",
  *   label = @Translation("User registration"),
  *   uri_paths = {
- *     "https://www.drupal.org/link-relations/create" = "/user/register",
+ *     "https://www.drupal.org/link-relations/create" = "/user/register/register",
  *   }
  * )
  */
 class UserRegistrationResource extends ResourceBase {
+  use EntityResourceValidationTrait;
 
   /**
    * Constructs a new UserRegistrationResource instance.
@@ -68,30 +68,94 @@ class UserRegistrationResource extends ResourceBase {
    *   Throws exception expected.
    */
   public function post($data) {
-    $user = $this->userStorage->load($data['uid']);
-
     try {
+      // Retrieve organization from the token.
+      $organization = \Drupal::service('anu_organization.organization')->getOrganizationFromToken($data['token']);
+      if (empty($organization)) {
+        return new ResourceResponse([
+          'message' => $this->t('Registration link is not valid. Please contact site administrator.'),
+        ], 406);
+      }
 
-      $user->setPassword($data['password_new']);
-      $user->_skipProtectedUserFieldConstraint = TRUE;
+      // Check if organization has not reached the limit.
+      $limit = (int) $organization->field_organization_limit->getString();
+      $amount = \Drupal::service('anu_organization.organization')->getRegisteredUsersAmount($organization->id());
+      if ($amount >= $limit) {
+        return new ResourceResponse([
+          'message' => $this->t('Organization has reached limit of users. Please contact site administrator.'),
+        ], 406);
+      }
+
+      // Check if email exists.
+      $email_taken = (bool) \Drupal::entityQuery('user')
+        ->condition('mail', $data['email'])
+        ->range(0, 1)
+        ->count()
+        ->execute();
+      if ($email_taken) {
+        return new ResourceResponse([
+          'message' => $this->t('The email is already taken.'),
+        ], 406);
+      }
+
+      // Check if username exists.
+      $username_taken = (bool) \Drupal::entityQuery('user')
+        ->condition('name', $data['username'])
+        ->range(0, 1)
+        ->count()
+        ->execute();
+      if ($username_taken) {
+        return new ResourceResponse([
+          'message' => $this->t('The username is already taken.'),
+        ], 406);
+      }
+
+      // Create a new user.
+      $user = $this->userStorage
+        ->create([
+          'name' => $data['username'],
+          'mail' => $data['email'],
+          'pass' => $data['password'],
+          'status' => 1,
+          'field_first_name' => $data['firstName'],
+          'field_last_name' => $data['lastName'],
+          'field_organization' => $organization->id(),
+        ]);
+      $user->enforceIsNew();
+
+      // Make sure that the user entity is valid (email and name are valid).
+      $this->validate($user);
+
       $user->save();
+
+      // Add user to classes.
+      $group_ids = \Drupal::entityQuery('group')
+        ->condition('field_organization', $organization->id())
+        ->execute();
+      if ($group_ids) {
+        $groups = \Drupal::entityTypeManager()
+          ->getStorage('group')
+          ->loadMultiple($group_ids);
+
+        foreach ($groups as $group) {
+          $group->addMember($user);
+        }
+      }
+
+      // Return registered user.
+      return new ResourceResponse(AnuNormalizerBase::normalizeEntity($user), 200);
     }
     catch (\Exception $e) {
-
       // Log an error.
       $message = $e->getMessage();
       if (empty($message)) {
-        $message = $this->t('Could not update password.');
+        $message = $this->t('Could not register a user.');
       }
       $this->logger->critical($message);
-      throw new HttpException(406, $message);
+      return new ResourceResponse([
+        'message' => $message,
+      ], 406);
     }
-
-    $response = new ResourceResponse(AnuNormalizerBase::normalizeEntity($user), 200);
-
-//    return new ResourceResponse([
-//      'message' => $this->t('Unable to reset password. Contact the site administrator if the problem persists.'),
-//    ], 406);
   }
 
 }
